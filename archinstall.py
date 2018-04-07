11d782baa6347df81be238daa68a28545d36a848
Lord Anton Hvornum <anton.feeds@gmail.com>
2018-04-07 18:09:59 +0200

    Wrong logical order on command failed logic

--- archinstall.py
+++ archinstall.py
@@ -267,7 +267,7 @@ if __name__ == '__main__':
 
 					print('[N] Command: {} ({})'.format(command, opts))
 					o = run('arch-chroot /mnt {c}'.format(c=command), echo=True, **opts)
-					if len(instructions[title][command]) and type(instructions[title][command]) == bytes and not instructions[title][command] in o:
+					if type(instructions[title][command]) == bytes and len(instructions[title][command]) and not instructions[title][command] in o:
 						print('[W] Post install command failed: {}'.format(o.decode('UTF-8')))
 					#print(o)
 
