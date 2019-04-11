074efd4c1c5ef5a4eb10fcf008a36886fb4e4587
Anton Hvornum <anton.feeds@gmail.com>
2019-04-11 22:18:50 +0200

    Live testing

--- archinstall.py
+++ archinstall.py
@@ -170,6 +170,7 @@ class sys_command():
 					for trigger in self.opts['triggers']:
 						print(trigger.lower(),'vs', lower)
 						if trigger.lower() in lower:
+							print('[N] Writing to subsystem: {}'.format(trigger))
 							os.write(child_fd, self.opts['triggers'][trigger])
 
 				yield output
