5e99895803accce102faeaac5544f76ed54a904d
Anton Hvornum <anton.feeds+github@gmail.com>
2019-11-23 21:38:30 +0000

    Fixed which statement. It's a builtin and not a /usr/bin command

--- archinstall.py
+++ archinstall.py
@@ -235,10 +235,7 @@ class sys_command():#Thread):
 
 		if not self.cmd[0][0] == '/':
 			log('Worker command is not executed with absolute path, trying to find: {}'.format(self.cmd[0]), origin='spawn', level=5)
-			x = Popen(f'/usr/bin/sh -c "which {self.cmd[0]}"', shell=True, stdout=PIPE)
-			while x.poll() is None:
-				pass
-			o = x.stdout.read()
+			o = check_output(['which', self.cmd[0]])
 			log('This is the binary {} for {}'.format(o.decode('UTF-8'), self.cmd[0]), origin='spawn', level=5)
 			self.cmd[0] = o.decode('UTF-8').strip()
 
