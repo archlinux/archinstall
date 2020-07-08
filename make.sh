4f18ac473abef763d6ea2846348da21693d878bd
Anton Hvornum <anton.feeds+github@gmail.com>
2020-07-08 12:35:02 +0000

    Removing temporary build scripts

--- make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf archinstall.egg-info/ build/ dist/
-python3 setup.py sdist bdist_wheel
-
-echo 'python3 -m twine upload dist/*'
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf archinstall.egg-info/ build/ dist/
-python3 setup.py sdist bdist_wheel
-sudo pip install --upgrade dist/*.whl
-ls -l /usr/lib/python3.8/site-packages/archinstall
-#echo 'python3 -m twine upload dist/*'
