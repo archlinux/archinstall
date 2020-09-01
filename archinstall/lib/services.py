1cdd96f9aadfb86540a1aa8303e85fbc7fbfb77c
Anton Hvornum <anton.feeds+github@gmail.com>
2020-09-01 14:43:04 +0200

    Renamed services again after re-save to the wrong name.

--- archinstall/lib/services.py
+++ archinstall/lib/services.py
@@ -4,7 +4,7 @@ from .exceptions import *
 from .general import *
 
 def service_state(service_name :str):
-	if os.path.fileext(service_name)[1] != '.service':
+	if os.path.splitext(service_name)[1] != '.service':
 		service_name += '.service' # Just to be safe
 
 	state = b''.join(sys_command(f'systemctl show -p SubState --value {service_name}'))
