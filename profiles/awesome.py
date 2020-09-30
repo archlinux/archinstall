cf3ccb478894d74a1e59980a23123cb8db191af3
Anton Hvornum <anton.feeds+github@gmail.com>
2020-09-30 20:47:28 +0200

    Cleaned up the awesome wm profile. A few things were test purpose-addons that shouldn't be in the default profile.

--- profiles/awesome.py
+++ profiles/awesome.py
@@ -31,7 +31,7 @@ if __name__ == 'awesome':
 
 	# Then setup and configure the desktop environment: awesome
 	arguments = {
-		'keyboard_layout' : 'sv-latin1',
+		#'keyboard_layout' : 'sv-latin1',
 		"editor" : "nano",
 		"mediaplayer" : "lollypop gstreamer gst-plugins-good gnome-keyring",
 		"filebrowser" : "nemo gpicview-gtk3",
@@ -44,12 +44,12 @@ if __name__ == 'awesome':
 
 	installation.add_additional_packages("{webbrowser} {utils} {mediaplayer} {window_manager} {virtulization} {filebrowser} {editor}".format(**arguments))
 
-	with open(f'{installation.mountpoint}/etc/X11/xinit/xinitrc', 'a') as X11:
-		X11.write('setxkbmap se\n')
+	#with open(f'{installation.mountpoint}/etc/X11/xinit/xinitrc', 'a') as X11:
+	#	X11.write('setxkbmap se\n')
 
-	with open(f'{installation.mountpoint}/etc/vconsole.conf', 'a') as vconsole:
-		vconsole.write('KEYMAP={keyboard_layout}\n'.format(**arguments))
-		vconsole.write('FONT=lat9w-16\n')
+	#with open(f'{installation.mountpoint}/etc/vconsole.conf', 'a') as vconsole:
+	#	vconsole.write('KEYMAP={keyboard_layout}\n'.format(**arguments))
+	#	vconsole.write('FONT=lat9w-16\n')
 
 
 	with open(f'{installation.mountpoint}/etc/xdg/awesome/rc.lua', 'r') as awesome_rc_lua:
