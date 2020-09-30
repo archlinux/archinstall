7f7cb6b89b38f32beb411e112001e3230aa972ed
Anton Hvornum <anton.feeds+github@gmail.com>
2020-09-30 11:52:28 +0000

    Forgot to replace the xinitrc data after replacing lines.

--- profiles/applications/awesome.py
+++ profiles/applications/awesome.py
@@ -6,10 +6,9 @@ with open(f'{installation.mountpoint}/etc/X11/xinit/xinitrc', 'r') as xinitrc:
 	xinitrc_data = xinitrc.read()
 
 for line in xinitrc_data.split('\n'):
-	print('twm &' in line, line)
-	if 'twm &' in line: xinitrc_data.replace(line, f"# {line}")
-	if 'xclock' in line: xinitrc_data.replace(line, f"# {line}")
-	if 'xterm' in line: xinitrc_data.replace(line, f"# {line}")
+	if 'twm &' in line: xinitrc_data = xinitrc_data.replace(line, f"# {line}")
+	if 'xclock' in line: xinitrc_data = xinitrc_data.replace(line, f"# {line}")
+	if 'xterm' in line: xinitrc_data = xinitrc_data.replace(line, f"# {line}")
 
 xinitrc_data += '\n'
 xinitrc_data += 'xscreensaver -no-splash &\n'
