4349512ef3f24c48c1c0903064d518e44c11101b
Anton Hvornum <anton@hvornum.se>
2021-02-07 18:53:55 +0100

    Added error handling for BlockDevice() that was given no information.

--- archinstall/lib/disk.py
+++ archinstall/lib/disk.py
@@ -14,7 +14,12 @@ GPT = 0b00000001
 #libc.mount.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_ulong, ctypes.c_char_p)
 
 class BlockDevice():
-	def __init__(self, path, info={}):
+	def __init__(self, path, info=None):
+		if not info:
+			# If we don't give any information, we need to auto-fill it.
+			# Otherwise any subsequent usage will break.
+			info = all_disks().get(path, {})
+
 		self.path = path
 		self.info = info
 		self.part_cache = OrderedDict()
