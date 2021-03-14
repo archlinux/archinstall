2ef7be9787c4d3acb91f91308acf7bf5c8715c1a
Anton Hvornum <anton@hvornum.se>
2021-03-14 12:17:30 +0100

    Wrong variable name.

--- archinstall/lib/exceptions.py
+++ archinstall/lib/exceptions.py
@@ -7,10 +7,10 @@ class UnknownFilesystemFormat(BaseException):
 class ProfileError(BaseException):
 	pass
 class SysCallError(BaseException):
-	def __init__(self, message, error_code):
+	def __init__(self, message, exit_code):
 		super(SysCallError, self).__init__(message)
 		self.message = message
-		self.error_code = error_code
+		self.exit_code = exit_code
 class ProfileNotFound(BaseException):
 	pass
 class HardwareIncompatibilityError(BaseException):
