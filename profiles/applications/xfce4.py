dcdd66fd29676709e9205b7983ca2ca7e882db62
m1ten <57693631+m1ten@users.noreply.github.com>
2021-04-03 16:19:23 -0400

    applications/xfce4

---
+++ profiles/applications/xfce4.py
@@ -0,0 +1,3 @@
+import archinstall
+
+installation.add_additional_packages("xfce4 xfce4-goodies lightdm") # We'll create a xfce4-minimal later, but for now, we'll avoid issues by giving more than we need.
