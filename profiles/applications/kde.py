5057035f3ed9ad14630c99f90a9bd6ffe095704e
advaithm <advaith.madhukar@gmail.com>
2021-04-06 18:28:18 +0530

    ask user for default session over asking if they want wayland

--- profiles/applications/kde.py
+++ profiles/applications/kde.py
@@ -1,2 +1,2 @@
 import archinstall
-installation.add_additional_packages("plasma-meta kde-applications-meta sddm") # We'll support plasma-desktop (minimal) later iirc sddm should be part of plasma-meta
+installation.add_additional_packages("plasma-meta kde-applications-meta plasma-wayland-session sddm") # We'll support plasma-desktop (minimal) later iirc sddm should be part of plasma-meta
