2963c342433f92f20df2fe40dff6287399bbea6e
Dylan Taylor <dylan@dylanmtaylor.com>
2021-04-05 20:45:59 -0400

    Combine KDE Wayland and Xorg installations into one profile.

--- profiles/applications/kde.py
+++ profiles/applications/kde.py
@@ -1,2 +1,6 @@
 import archinstall
-installation.add_additional_packages("plasma-meta kde-applications-meta sddm") # We'll support plasma-desktop (minimal) later iirc sddm should be part of plasma-meta
+packages = "plasma-meta kde-applications-meta plasma-wayland-session sddm"
+# if the package selection can be reduced go for it
+if "nvidia" in _gfx_driver_packages:
+	packages = packages + " egl-wayland"
+installation.add_additional_packages(packages)
