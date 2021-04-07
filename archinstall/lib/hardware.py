5b7b66cc0e044c7c3bff5a1d8ddb7332ac22c0f0
advaithm <advaith.madhukar@gmail.com>
2021-04-07 07:15:12 +0530

    added return values for functions and cpuVendor function

--- archinstall/lib/hardware.py
+++ archinstall/lib/hardware.py
@@ -1,18 +1,18 @@
-import os, subprocess
+import os, subprocess, json
 from .general import sys_command
 from .networking import list_interfaces, enrichIfaceTypes
 
-def hasWifi():
+def hasWifi()->bool:
 	if 'WIRELESS' in enrichIfaceTypes(list_interfaces().values()).values():
 		return True
 	return False
 
-def hasAMDCPU():
+def hasAMDCPU()->bool:
 	if subprocess.check_output("lscpu | grep AMD", shell=True).strip().decode():
 		return True
 	return False
 
-def hasUEFI():
+def hasUEFI()->bool:
 	return os.path.isdir('/sys/firmware/efi')
 
 def graphicsDevices():
@@ -23,19 +23,27 @@ def graphicsDevices():
 			cards[identifier.strip().lower().decode('UTF-8')] = line
 	return cards
 
-def hasNvidiaGraphics():
+def hasNvidiaGraphics()->bool:
 	if [x for x in graphicsDevices() if 'nvidia' in x]:
 		return True
 	return False
 
-def hasAmdGraphics():
+def hasAmdGraphics()->bool:
 	if [x for x in graphicsDevices() if 'amd' in x]:
 		return True
 	return False
 
-def hasIntelGraphics():
+def hasIntelGraphics()->bool:
 	if [x for x in graphicsDevices() if 'intel' in x]:
 		return True
 	return False
+
+def cpuVendor():
+	cpu_info = json.loads(subprocess.check_output("lscpu -J", shell=True).decode('utf-8'))['lscpu']
+	for info in cpu_info:
+		if info.get('field',None):
+			if info.get('field',None) == "Vendor ID:":
+				return info.get('data',None)
+
 
 # TODO: Add more identifiers
