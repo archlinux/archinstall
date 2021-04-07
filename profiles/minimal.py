608bada172ca0504b8aa7667e876f87d10449deb
Dylan M. Taylor <dylan@dylanmtaylor.com>
2021-04-07 11:54:10 -0400

    Clean up comments a bit

--- profiles/minimal.py
+++ profiles/minimal.py
@@ -7,15 +7,14 @@ is_top_level_profile = True
 def _prep_function(*args, **kwargs):
 	"""
 	Magic function called by the importing installer
-	before continuing any further. It also avoids executing any
-	other code in this stage. So it's a safe way to ask the user
-	for more input before any other installer steps start.
+	before continuing any further. For minimal install,
+	we don't need to do anything special here, but it
+	needs to exist and return True.
 	"""
-	return True # Do nothing here for now
+	return True # Do nothing and just return True
 
 if __name__ == 'minimal':
 	"""
 	This "profile" is a meta-profile.
 	It is used for a custom minimal installation, without any desktop-specific packages.
 	"""
-	# Do nothing here for now
