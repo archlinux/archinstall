dbea18dab868dd4e32a2e72628dcb75a159f87a9
Anton Hvornum <anton.feeds+github@gmail.com>
2021-04-07 09:28:16 +0200

    Switched from print to installation.log

--- examples/guided.py
+++ examples/guided.py
@@ -282,7 +282,7 @@ def perform_installation(device, boot_partition, language, mirrors):
 				installation.enable_service('systemd-networkd')
 				installation.enable_service('systemd-resolved')
 
-			print('This audio server will be used: ' + archinstall.arguments.get('audio', None))
+			installation.log(f"The {archinstall.arguments.get('audio', None)} audio server will be used.", level=archinstall.LOG_LEVELS.Info)
 			if archinstall.arguments.get('audio', None) == 'pipewire':
 				print('Installing pipewire ...')
 				installation.add_additional_packages(["pipewire", "pipewire-alsa", "pipewire-docs", "pipewire-jack", "pipewire-media-session", "pipewire-pulse", "gst-plugin-pipewire", "libpulse"])
