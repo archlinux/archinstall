9545680459528c63e9dececb0804d9b498130ef6
Dylan M. Taylor <dylan@dylanmtaylor.com>
2021-04-08 16:21:46 -0400

    breeze-icons and oxygen-icons are both needed to not have missing icons

--- profiles/applications/lxqt.py
+++ profiles/applications/lxqt.py
@@ -1,3 +1,3 @@
 import archinstall
 
-installation.add_additional_packages("lxqt breeze-icons xdg-utils ttf-freefont leafpad xscreensaver archlinux-wallpaper sddm")
+installation.add_additional_packages("lxqt breeze-icons oxygen-icons xdg-utils ttf-freefont leafpad xscreensaver archlinux-wallpaper sddm")
