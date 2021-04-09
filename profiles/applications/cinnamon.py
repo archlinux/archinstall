326b96d80869bc9c67eec89eb05bea2fac131939
Dylan Taylor <dylan@dylanmtaylor.com>
2021-04-09 08:29:58 -0400

    Add budgie implementation

--- profiles/applications/cinnamon.py
+++ profiles/applications/cinnamon.py
@@ -1,4 +1,3 @@
 import archinstall
 
 installation.add_additional_packages("cinnamon system-config-printer gnome-keyring gnome-terminal blueberry metacity lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings") 
-# We'll create a cinnamon-minimal later, but for now, we'll avoid issues by giving more than we need.
--- profiles/applications/xfce4.py
+++ profiles/applications/xfce4.py
@@ -1,4 +1,3 @@
 import archinstall
 
 installation.add_additional_packages("xfce4 xfce4-goodies lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings") 
-# We'll create a xfce4-minimal later, but for now, we'll avoid issues by giving more than we need.
