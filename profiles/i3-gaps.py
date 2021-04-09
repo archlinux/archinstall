82c5241946ec96223aebe6453c58845182290fbb
Dylan Taylor <dylan@dylanmtaylor.com>
2021-04-08 21:23:29 -0400

    This might work to make the i3 profiles as small as possible

--- profiles/i3-gaps.py
+++ profiles/i3-gaps.py
@@ -9,14 +9,7 @@ def _prep_function(*args, **kwargs):
 	other code in this stage. So it's a safe way to ask the user
 	for more input before any other installer steps start.
 	"""
-
-	# i3 requires a functioning Xorg installation.
-	profile = archinstall.Profile(None, 'xorg')
-	with profile.load_instructions(namespace='xorg.py') as imported:
-		if hasattr(imported, '_prep_function'):
-			return imported._prep_function()
-		else:
-			print('Deprecated (??): xorg profile has no _prep_function() anymore')
+	return True
 
 if __name__ == 'i3-gaps':
     # install the i3 group now
--- profiles/i3.py
+++ profiles/i3.py
@@ -18,11 +18,19 @@ def _prep_function(*args, **kwargs):
 
 	supported_configurations = ['i3-wm', 'i3-gaps']
 	desktop = archinstall.generic_select(supported_configurations, 'Select your desired configuration: ')
-	
+
 	# Temporarily store the selected desktop profile
 	# in a session-safe location, since this module will get reloaded
 	# the next time it gets executed.
 	archinstall.storage['_desktop_profile'] = desktop
+
+	# i3 requires a functioning Xorg installation.
+	profile = archinstall.Profile(None, 'xorg')
+	with profile.load_instructions(namespace='xorg.py') as imported:
+		if hasattr(imported, '_prep_function'):
+			return imported._prep_function()
+		else:
+			print('Deprecated (??): xorg profile has no _prep_function() anymore')
 
 	profile = archinstall.Profile(None, desktop)
 	# Loading the instructions with a custom namespace, ensures that a __name__ comparison is never triggered.
