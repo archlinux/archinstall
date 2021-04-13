fad9f40a83877701163988ab20029aec93cfdb7b
Anton Hvornum <anton.feeds@gmail.com>
2021-04-13 13:36:40 +0200

    Modifying  to enable piping in custom environment variables. This is required to disable systemd coloring and paging when querying for service states. Otherwise it returns unreliable data that can cause hanging.

--- archinstall/lib/services.py
+++ archinstall/lib/services.py
@@ -7,6 +7,6 @@ def service_state(service_name: str):
 	if os.path.splitext(service_name)[1] != '.service':
 		service_name += '.service'  # Just to be safe
 
-	state = b''.join(sys_command(f'systemctl show -p SubState --value {service_name}'))
+	state = b''.join(sys_command(f'systemctl show --no-pager -p SubState --value {service_name}', environment_vars={'SYSTEMD_COLORS' : '0'}))
 
 	return state.strip().decode('UTF-8')
