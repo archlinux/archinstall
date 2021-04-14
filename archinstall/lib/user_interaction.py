8e9a09a8ff9588490375a26013777d6920300744
advaithm <advaith.madhukar@gmail.com>
2021-04-14 14:18:18 +0530

    typo

--- archinstall/lib/user_interaction.py
+++ archinstall/lib/user_interaction.py
@@ -357,7 +357,7 @@ def select_language(options, show_only_country_codes=True):
 			selected_language = options[options.index(selected_language)]
 			return selected_language
 		else:
-			print("Invalid Langue please select a valid option.")
+			print("Invalid Language please select a valid option.")
 
 	raise RequirementError("Selecting languages require a least one language to be given as an option.")
 
