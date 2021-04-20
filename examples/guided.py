dbb6d630bdfdb655a3567b8cebc5c6b82bb74459
advaithm <advaith.madhukar@gmail.com>
2021-04-20 19:43:23 +0530

    fixed some issues with adding packages

--- examples/guided.py
+++ examples/guided.py
@@ -306,9 +306,9 @@ def perform_installation(mountpoint):
 		if archinstall.arguments.get('mirror-region', None):
 			archinstall.use_mirrors(archinstall.arguments['mirror-region']) # Set the mirrors for the live medium
 		if hasUEFI()==False:
-			installation.base_packages.replace("efibootmgr","")# if we aren't on a uefi system why install efibootmgr
+			installation.base_packages.pop(installation.base_packages.index("efibootmgr"))# if we aren't on a uefi system why install efibootmgr
 		if archinstall.arguments["bootloader"] == "grub-install":
-			installation.base_packages = installation.base_packages + " grub"
+			installation.base_packages = installation.base_packages.append("grub")
 		if installation.minimal_installation():
 			installation.set_hostname(archinstall.arguments['hostname'])
 			if archinstall.arguments['mirror-region'].get("mirrors",{})!= None:
