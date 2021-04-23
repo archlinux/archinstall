b2ae102adb498ae2ab202b53115fd1cce5f08ee7
Yash Tripathi <yashtripathi@fico.com>
2021-04-23 09:59:02 +0530

    added basic exception handling while parsing file

--- archinstall/__init__.py
+++ archinstall/__init__.py
@@ -28,7 +28,10 @@ parser = ArgumentParser()
 parser.add_argument('--file', help='json config file', type=FileType('r'))
 args = parser.parse_args()
 with open(args.file) as file:
-    arguments = json.load(file)
+    try:
+        arguments = json.load(file)
+    except Exception as e:
+        print(e)
 
 for arg in sys.argv[1:]:
     if '--' == arg[:2]:
