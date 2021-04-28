a7cc83db49f7687508de4204ce08f30f371c3480
Dylan M. Taylor <dylan@dylanmtaylor.com>
2021-04-28 10:58:42 -0400

    Convert budgie.py to using packages definition

--- profiles/applications/budgie.py
+++ profiles/applications/budgie.py
@@ -1,4 +1,6 @@
 import archinstall
 
 # "It is recommended also to install the gnome group, which contains applications required for the standard GNOME experience." - Arch Wiki 
-installation.add_additional_packages("budgie-desktop lightdm lightdm-gtk-greeter gnome")
+__packages__ = ["budgie-desktop", "lightdm", "lightdm-gtk-greeter", "gnome"]
+
+installation.add_additional_packages(__packages__)
