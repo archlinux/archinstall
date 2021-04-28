f9f359ab73dabb74543909af5e98adb6c56a189d
Dylan M. Taylor <dylan@dylanmtaylor.com>
2021-04-28 11:30:30 -0400

    This variable wasn't really needed.

--- profiles/applications/kde.py
+++ profiles/applications/kde.py
@@ -1,8 +1,6 @@
 import archinstall
 
-packages = "" # Other packages for KDE are installed in the main profile now.
+# Other packages for KDE are installed in the main profile now.
 
 if "nvidia" in _gfx_driver_packages:
-	packages = packages + " egl-wayland"
-
-installation.add_additional_packages(packages)
+	installation.add_additional_packages("egl-wayland")
