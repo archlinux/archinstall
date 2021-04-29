10505ac42aa2a83919aadb951be9de3b4c722698
Anton Hvornum <anton.feeds@gmail.com>
2021-04-29 13:08:05 +0200

    Removed some debugging

--- archinstall/lib/profiles.py
+++ archinstall/lib/profiles.py
@@ -137,10 +137,6 @@ class Script():
 		if namespace:
 			self.namespace = namespace
 
-		if self.namespace == 'awesome':
-			raise ValueError("mooo")
-		print(f'Loading {self} with namespace: {self.namespace}')
-
 		self.spec = importlib.util.spec_from_file_location(self.namespace, self.path)
 		imported = importlib.util.module_from_spec(self.spec)
 		sys.modules[self.namespace] = imported
