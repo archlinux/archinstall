25b699b44ea272e9bac7d75c7d86871e082122ae
Anton Hvornum <anton@hvornum.se>
2021-04-30 15:33:26 +0200

    Automatic loading of plugins

--- archinstall/lib/plugins.py
+++ archinstall/lib/plugins.py
@@ -1,1 +1,13 @@
+import pkgutil
+import importlib
+import imp # Deprecated
+
 plugins = {}
+
+for module_info in pkgutil.iter_modules(path=None, prefix=''):
+	if 'archinstall-' in module_info.name and module_info.ispkg:
+		try:
+			modulesource = importlib.import_module(module_info.name)
+			imp.reload(modulesource)
+		except Exception as e:
+			print('Could not load plugin {} {}'.format(modname, e))
