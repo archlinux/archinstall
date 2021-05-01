d27fe9715f357d0a3d98ed166f480d27de3e920f
Dylan M. Taylor <dylan@dylanmtaylor.com>
2021-05-01 09:28:42 -0400

    Attempt to fix #406 I think this should fix the comment Torxed made

--- profiles/sway.py
+++ profiles/sway.py
@@ -4,7 +4,19 @@ import archinstall
 
 is_top_level_profile = False
 
-__packages__ = ["sway", "swaylock", "swayidle", "waybar", "dmenu", "light", "grim", "slurp", "pavucontrol", "alacritty"]
+__packages__ = [
+	"sway",
+	"swaylock",
+	"swayidle",
+	"waybar",
+	"dmenu",
+	"light",
+	"grim",
+	"slurp",
+	"pavucontrol",
+	"alacritty",
+]
+
 
 def _prep_function(*args, **kwargs):
 	"""
@@ -13,18 +25,26 @@ def _prep_function(*args, **kwargs):
 	other code in this stage. So it's a safe way to ask the user
 	for more input before any other installer steps start.
 	"""
-	if "nvidia" in _gfx_driver_packages:
-		choice = input("The proprietary Nvidia driver is not supported by Sway. It is likely that you will run into issues. Continue anyways? [y/N] ")
-		if choice.lower() in ("n", ""):
-			raise archinstall.lib.exceptions.HardwareIncompatibilityError("Sway does not support the proprietary nvidia drivers.")
-
-	__builtins__['_gfx_driver_packages'] = archinstall.select_driver()
+	__builtins__["_gfx_driver_packages"] = archinstall.select_driver()
 
 	return True
+
 
 # Ensures that this code only gets executed if executed
 # through importlib.util.spec_from_file_location("sway", "/somewhere/sway.py")
 # or through conventional import sway
-if __name__ == 'sway':
+if __name__ == "sway":
+	if "nvidia" in _gfx_driver_packages:
+		choice = input(
+			"The proprietary Nvidia driver is not supported by Sway. It is likely that you will run into issues. Continue anyways? [y/N] "
+		)
+		if choice.lower() in ("n", ""):
+			raise archinstall.lib.exceptions.HardwareIncompatibilityError(
+				"Sway does not support the proprietary nvidia drivers."
+			)
+
 	# Install the Sway packages
 	installation.add_additional_packages(__packages__)
+
+	# Install the graphics driver packages
+	installation.add_additional_packages(_gfx_driver_packages)
