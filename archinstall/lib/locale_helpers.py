ec04dc2e0e6bdb0aa5bc03eb738eaa9abb4d0ad3
Anton Hvornum <anton@hvornum.se>
2021-05-15 14:51:30 +0200

    Moving to localectl rather than local file listing, this when trying to list available locales.

--- archinstall/lib/locale_helpers.py
+++ archinstall/lib/locale_helpers.py
@@ -2,19 +2,11 @@ import subprocess
 import os
 
 from .exceptions import *
-# from .general import sys_command
+from .general import sys_command
 
 def list_keyboard_languages():
-	locale_dir = '/usr/share/kbd/keymaps/'
-
-	if not os.path.isdir(locale_dir):
-		raise RequirementError(f'Directory containing locales does not exist: {locale_dir}')
-
-	for root, folders, files in os.walk(locale_dir):
-
-		for file in files:
-			if os.path.splitext(file)[1] == '.gz':
-				yield file.strip('.gz').strip('.map')
+	for line in sys_command("localectl --no-pager list-keymaps"):
+		yield line.decode('UTF-8').strip()
 
 def verify_keyboard_layout(layout):
 	for language in list_keyboard_languages():
