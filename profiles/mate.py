0ac13c0958f597156e5e383587bad8a75ac9d870
Dylan Taylor <dylan@dylanmtaylor.com>
2021-05-15 17:35:21 -0400

    Missed a couple of profiles; reformat package lists for these too.

--- profiles/mate.py
+++ profiles/mate.py
@@ -4,7 +4,12 @@ import archinstall
 
 is_top_level_profile = False
 
-__packages__ = ["mate", "mate-extra", "lightdm", "lightdm-gtk-greeter"]
+__packages__ = [
+	"mate",
+	"mate-extra",
+	"lightdm",
+	"lightdm-gtk-greeter",
+]
 
 
 def _prep_function(*args, **kwargs):
--- profiles/xfce4.py
+++ profiles/xfce4.py
@@ -4,7 +4,12 @@ import archinstall
 
 is_top_level_profile = False
 
-__packages__ = ["xfce4", "xfce4-goodies", "lightdm", "lightdm-gtk-greeter"]
+__packages__ = [
+	"xfce4",
+	"xfce4-goodies",
+	"lightdm",
+	"lightdm-gtk-greeter",
+]
 
 
 def _prep_function(*args, **kwargs):
