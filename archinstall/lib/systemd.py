60072df767e684a10ec98a2df25b0e4dd5b1b0c4
Anton Hvornum <anton.feeds@gmail.com>
2021-05-18 12:07:22 +0200

    Adding a systemd Boot() function for installations. This needs to be brought back in order to facilitate certain calls such as localectl for installations, since it uses debus to communicate with systemd - systemd needs to be booted internally to facilitate the calls. systemd-nspawn does a good job here, even if it's a bit 'slow'

--- archinstall/lib/systemd.py
+++ archinstall/lib/systemd.py
@@ -1,3 +1,9 @@
+import logging
+
+from .installer import Installer
+from .general import SysCommand
+from .output import log
+
 class Ini:
 	def __init__(self, *args, **kwargs):
 		"""
@@ -36,3 +42,26 @@ class Networkd(Systemd):
 	"""
 	Placeholder class to do systemd-network specific setups.
 	"""
+
+class Boot:
+	def __init__(installation :Installer):
+		self.instance = installation
+		self.session = None
+
+	def __enter__(self):
+		self.session = SysCommand([
+			'/usr/bin/systemd-nspawn',
+			'-D', installation.target,
+			'-b',
+			'--machine', 'temporary'
+		])
+
+		return self
+
+	def __exit__(self, *args, **kwargs):
+		# b''.join(sys_command('sync')) # No need to, since the underlying fs() object will call sync.
+		# TODO: https://stackoverflow.com/questions/28157929/how-to-safely-handle-an-exception-inside-a-context-manager
+
+		if len(args) >= 2 and args[1]:
+			log(args[1], level=logging.ERROR, fg='red')
+			log(f"The error above occured in a temporary boot-up of the installation {installation}", level=logging.ERROR, fg="red")
