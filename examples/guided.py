bc58ec047d0c2eb08e2b6178ac278c8fd889e3db
Dylan Taylor <dylan@dylanmtaylor.com>
2021-05-20 18:39:36 -0400

    Re-add profile import

--- examples/guided.py
+++ examples/guided.py
@@ -7,6 +7,7 @@ import archinstall
 from archinstall.lib.general import run_custom_user_commands
 from archinstall.lib.hardware import has_uefi
 from archinstall.lib.networking import check_mirror_reachable
+from archinstall.lib.profiles import Profile
 
 if archinstall.arguments.get('help'):
 	print("See `man archinstall` for help.")
