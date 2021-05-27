c3b703bc2ac51c4666ee6aed3586d28fd9ecd1af
nullrequest <30698906+advaithm@users.noreply.github.com>
2021-05-27 09:34:55 +0530

    Make it more clear what the desktop profile does

--- archinstall/lib/user_interaction.py
+++ archinstall/lib/user_interaction.py
@@ -579,6 +579,7 @@ def select_profile():
 
 		print(' -- The above list is a set of pre-programmed profiles. --')
 		print(' -- They might make it easier to install things like desktop environments. --')
+		print(' -- The desktop profile contains a list of profiles for DEs/WMs, e.g sway,kde plasma and gnome --')
 		print(' -- (Leave blank and hit enter to skip this step and continue) --')
 
 		selected_profile = generic_select(actual_profiles_raw, 'Enter a pre-programmed profile name if you want to install one: ', options_output=False)
