baca0cc51456c6129f3cb13b508e66687023f64c
Anton Hvornum <anton.feeds@gmail.com>
2021-05-31 10:09:03 +0200

    Creating PR

--- archinstall/lib/installer.py
+++ archinstall/lib/installer.py
@@ -468,7 +468,7 @@ class Installer:
 				entry.write('# Created by: archinstall\n')
 				entry.write(f'# Created on: {self.init_time}\n')
 				entry.write('title Arch Linux\n')
-				entry.write('linux /vmlinuz-linux\n')
+				entry.write('linux /vmlinuz-linux\n') # Issue: hardcoded
 				if not is_vm():
 					vendor = cpu_vendor()
 					if vendor == "AuthenticAMD":
