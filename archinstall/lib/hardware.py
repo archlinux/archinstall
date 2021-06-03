118cc17eb210a29b61406f6a5bd7d68cbc0744eb
Dylan Taylor <dylan@dylanmtaylor.com>
2021-06-02 21:26:24 -0400

    Fix my Quadro T1000 not being detected

--- archinstall/lib/hardware.py
+++ archinstall/lib/hardware.py
@@ -79,7 +79,7 @@ def has_uefi() -> bool:
 def graphics_devices() -> dict:
 	cards = {}
 	for line in SysCommand("lspci"):
-		if b' VGA ' in line:
+		if b' VGA ' in line or b' 3D ' in line:
 			_, identifier = line.split(b': ', 1)
 			cards[identifier.strip().lower().decode('UTF-8')] = line
 	return cards
