2cfbafc6534ebcca7e8d52be263aed4d54c04914
Anton Hvornum <anton@hvornum.se>
2021-09-15 19:16:30 +0200

    More error handling and fixed a spelling error.

--- examples/guided.py
+++ examples/guided.py
@@ -56,14 +56,17 @@ def load_config():
 		archinstall.storage['_selected_servers'] = archinstall.arguments.get('servers', None)
 	if archinstall.arguments.get('disk_layouts', None) is not None:
 		if (dl_path := pathlib.Path(archinstall.arguments['disk_layouts'])).exists() and str(dl_path).endswith('.json'):
-			with open(dl_path) as fh:
-				archinstall.arguments['disk_layouts'] = json.load(fh)
+			try:
+				with open(dl_path) as fh:
+					archinstall.arguments['disk_layouts'] = json.load(fh)
+			except Exception as e:
+				raise ValueError(f"--disk_layouts does not contain a valid JSON format: {e}")
 		else:
 			try:
 				archinstall.arguments['disk_layouts'] = json.loads(archinstall.arguments['disk_layouts'])
 			except:
 				raise ValueError("--disk_layouts=<json> needs either a JSON file or a JSON string given with a valid disk layout.")
-		archinstall.storage['disk_layouts'] = {archinstall.BlockDevice(disk) : struct for disk, struct in archinstall.arguments['disk_layouts']}
+		archinstall.storage['disk_layouts'] = {archinstall.BlockDevice(disk) : struct for disk, struct in archinstall.arguments['disk_layouts'].items()}
 
 	
 def ask_user_questions():
