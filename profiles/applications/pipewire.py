b74d9a5640ca5324233404d3ff46584720df99e5
Anton Hvornum <anton@hvornum.se>
2022-01-02 16:30:45 +0100

    Tweaked debugging

--- profiles/applications/pipewire.py
+++ profiles/applications/pipewire.py
@@ -1,13 +1,14 @@
 import archinstall
+import logging
 
 # Define the package list in order for lib to source
 # which packages will be installed by this profile
 __packages__ = ["pipewire", "pipewire-alsa", "pipewire-jack", "pipewire-media-session", "pipewire-pulse", "gst-plugin-pipewire", "libpulse"]
 
-print('Installing pipewire ...')
-print(archinstall.storage)
+archinstall.log('Installing pipewire', level=logging.INFO)
 archinstall.storage['installation_session'].add_additional_packages(__packages__)
 
 @archinstall.plugin
 def on_user_created(installation :archinstall.Installer, user :str):
+	archinstall.log(f"Enabling pipewire-pulse for {user}", level=logging.INFO)
 	installation.chroot('systemctl enable --user pipewire-pulse.service', run_as=user)
