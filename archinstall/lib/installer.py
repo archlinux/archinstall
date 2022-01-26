edabd7f1a47ae09b36a00556aba01b426a7e713d
Anton Hvornum <anton@hvornum.se>
2022-01-26 09:11:09 +0100

    Testing GRUB with /boot/efi instead (might revert)

--- archinstall/lib/installer.py
+++ archinstall/lib/installer.py
@@ -735,10 +735,10 @@ class Installer:
 			log(f"GRUB uses {boot_partition.path} as the boot partition.", level=logging.INFO)
 			if has_uefi():
 				self.pacstrap('efibootmgr') # TODO: Do we need? Yes, but remove from minimal_installation() instead?
-				if not (handle := SysCommand(f'/usr/bin/arch-chroot {self.target} grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB')).exit_code == 0:
-					log(f"Grub output during UEFI install: {handle}")
-					time.sleep(5)
+				if not (handle := SysCommand(f'/usr/bin/arch-chroot {self.target} grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB')).exit_code == 0:
 					raise DiskError(f"Could not install GRUB to {self.target}/boot: {handle}")
+				log(f"Grub output during UEFI install: {handle}")
+				time.sleep(5)
 			else:
 				if not (handle := SysCommand(f'/usr/bin/arch-chroot {self.target} grub-install --target=i386-pc --recheck {boot_partition.parent}')).exit_code == 0:
 					raise DiskError(f"Could not install GRUB to {boot_partition.path}: {handle}")
