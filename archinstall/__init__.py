3cd7dc24c7ebe8911978380d75fd79e0c581060a
Werner Llácer <wllacer@gmail.com>
2022-02-03 00:02:30 +0100

    Command locales (second batch) (#886)
    * flexibilize the definition of execution locale for OS commands executed via the SysCommand* interface.
    Defined a storage argument which holds the default
    Added functions to
    unset the program own locales
    reset to the program default locales
    set a specific locale
    A decorator to execute functions in the host locale environment
    
    * rename decorator local_environ to host_locale_environ
    created a simmetric decorator c_locale_environ, to make a routine work with the C locale whatever is set
    
    * Correct definition of btrfs standard layout
    
    * Added error handling
    
    * Fixed issue where archinstall.Boot() would raise an exception in vain
    
    * Added debugging for SysCommandWorker()
    
    * Added some debugging
    
    * Tweaking debug a bit
    
    * Tweaking debug
    
    * Adding more debug
    
    * Adding more debug
    
    * Removed some debugging
    
    * Adding more debug
    
    * Adding more debug
    
    * Adding more debug
    
    * Adding more debug
    
    * Adding more debug
    
    * Adding more debug
    
    * Adding more debug
    
    * Adding more debug
    
    * Adding more debug
    
    * Adding more debug
    
    * Adding more debug
    
    * Removed soem debugging
    
    * Removed soem debugging
    
    * Testing a revert
    
    * Adding back the reverted change, adding lofile
    
    * Redirecting stdout to /dev/null for testing (to avoid interrupting the fork)
    
    * Reverted debug changes
    
    * Testing os.system()
    
    Co-authored-by: Anton Hvornum <anton@hvornum.se>

--- archinstall/__init__.py
+++ archinstall/__init__.py
@@ -58,12 +58,12 @@ def define_arguments():
 					help="JSON disk layout file")
 	parser.add_argument("--silent", action="store_true",
 						help="WARNING: Disables all prompts for input and confirmation. If no configuration is provided, this is ignored")
-	parser.add_argument("--dry-run","--dry_run",action="store_true",
+	parser.add_argument("--dry-run", "--dry_run", action="store_true",
 						help="Generates a configuration file and then exits instead of performing an installation")
 	parser.add_argument("--script", default="guided", nargs="?", help="Script to run for installation", type=str)
-	parser.add_argument("--mount-point","--mount_point",nargs="?",type=str,help="Define an alternate mount point for installation")
-	parser.add_argument("--debug",action="store_true",help="Adds debug info into the log")
-	parser.add_argument("--plugin",nargs="?",type=str)
+	parser.add_argument("--mount-point","--mount_point", nargs="?", type=str, help="Define an alternate mount point for installation")
+	parser.add_argument("--debug", action="store_true", default=False, help="Adds debug info into the log")
+	parser.add_argument("--plugin", nargs="?", type=str)
 
 def parse_unspecified_argument_list(unknowns :list, multiple :bool = False, error :bool = False) -> dict:
 	"""We accept arguments not defined to the parser. (arguments "ad hoc").
@@ -170,7 +170,7 @@ def post_process_arguments(arguments):
 	if arguments.get('mount_point'):
 		storage['MOUNT_POINT'] = arguments['mount_point']
 
-	if arguments.get('debug',False):
+	if arguments.get('debug', False):
 		log(f"Warning: --debug mode will write certain credentials to {storage['LOG_PATH']}/{storage['LOG_FILE']}!", fg="red", level=logging.WARNING)
 
 	if arguments.get('plugin', None):
