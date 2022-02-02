fb6272231e8a1ea4aefbf693b36239e00cce8b2b
Anton Hvornum <anton@hvornum.se>
2022-02-02 08:03:48 +0100

    Added a by-path import to support git clone - import of the library

--- archinstall/__main__.py
+++ archinstall/__main__.py
@@ -1,4 +1,15 @@
-import archinstall
+import importlib
+import sys
+import pathlib
+
+# Load .git version before the builtin version
+if pathlib.Path('./archinstall/__init__.py').absolute().exists():
+	spec = importlib.util.spec_from_file_location("archinstall", "./archinstall/__init__.py")
+	archinstall = importlib.util.module_from_spec(spec)
+	sys.modules["archinstall"] = archinstall
+	spec.loader.exec_module(sys.modules["archinstall"])
+else:
+	import archinstall
 
 if __name__ == '__main__':
 	archinstall.run_as_a_module()
