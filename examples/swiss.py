d7162f4015a1a795729c2d897bb2ff60525c36a4
Werner Llácer <wllacer@gmail.com>
2022-02-05 16:50:20 +0100

    git complains

--- examples/swiss.py
+++ examples/swiss.py
@@ -490,6 +490,7 @@ def perform_installation(mountpoint, mode):
 	# For support reasons, we'll log the disk layout post installation (crash or no crash)
 	archinstall.log(f"Disk states after installing: {archinstall.disk_layouts()}", level=logging.DEBUG)
 
+
 if not archinstall.check_mirror_reachable():
 	log_file = os.path.join(archinstall.storage.get('LOG_PATH', None), archinstall.storage.get('LOG_FILE', None))
 	archinstall.log(f"Arch Linux mirrors are not reachable. Please check your internet connection and the log file '{log_file}'.", level=logging.INFO, fg="red")
