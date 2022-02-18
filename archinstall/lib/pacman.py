1ae4e99474a829be5dfcf1330a1eb2522f669028
Daniel Girtler <girtler.daniel@gmail.com>
2022-02-18 08:53:11 +0100

    Fix flake8

--- archinstall/lib/pacman.py
+++ archinstall/lib/pacman.py
@@ -13,15 +13,15 @@ def run_pacman(args :str, default_cmd :str = 'pacman') -> SysCommand:
 	The grace period is set to 10 minutes before exiting hard if another pacman instance is running.
 	"""
 	pacman_db_lock = pathlib.Path('/var/lib/pacman/db.lck')
-	
+
 	if pacman_db_lock.exists():
 		log(_('Pacman is already running, waiting maximum 10 minutes for it to terminate.'), level=logging.WARNING, fg="red")
 
 	started = time.time()
 	while pacman_db_lock.exists():
 		time.sleep(0.25)
-		
-		if time.time() - started > (60*10):
+
+		if time.time() - started > (60 * 10):
 			log(_('Pre-existing pacman lock never exited. Please clean up any existing pacman sessions before using archinstall.'), level=logging.WARNING, fg="red")
 			exit(1)
 
