77bfa10d53bb0b18fa775b06596c6463e06e602b
Anton Hvornum <anton@hvornum.se>
2022-03-28 14:43:33 +0200

    Added error handling to fstab (#1045)
    * Added error handling to fstab
    
    * Checking the exit code for == 0

--- archinstall/lib/installer.py
+++ archinstall/lib/installer.py
@@ -380,11 +380,14 @@ class Installer:
 	def genfstab(self, flags :str = '-pU') -> bool:
 		self.log(f"Updating {self.target}/etc/fstab", level=logging.INFO)
 
+		if not (fstab := SysCommand(f'/usr/bin/genfstab {flags} {self.target}')).exit_code == 0:
+			raise RequirementError(f'Could not generate fstab, strapping in packages most likely failed (disk out of space?)\n Error: {fstab}')
+
 		with open(f"{self.target}/etc/fstab", 'a') as fstab_fh:
-			fstab_fh.write((fstab := SysCommand(f'/usr/bin/genfstab {flags} {self.target}')).decode())
+			fstab_fh.write(fstab.decode())
 
 		if not os.path.isfile(f'{self.target}/etc/fstab'):
-			raise RequirementError(f'Could not generate fstab, strapping in packages most likely failed (disk out of space?)\n{fstab}')
+			raise RequirementError(f'Could not generate fstab, strapping in packages most likely failed (disk out of space?)\n Error: {fstab}')
 
 		for plugin in plugins.values():
 			if hasattr(plugin, 'on_genfstab'):
