f525200d00adf9495833a323bd8f85d47b8e3a2d
Anton Hvornum <anton@hvornum.se>
2022-05-23 00:23:47 +0200

    Added imports

--- archinstall/lib/disk/btrfs/__init__.py
+++ archinstall/lib/disk/btrfs/__init__.py
@@ -13,7 +13,8 @@ from .btrfs_helpers import (
 	get_subvolumes_from_findmnt as get_subvolumes_from_findmnt,
 	subvolume_info_from_path as subvolume_info_from_path,
 	find_parent_subvolume as find_parent_subvolume,
-	setup_subvolume as setup_subvolume
+	setup_subvolume as setup_subvolume,
+	mount_subvolume_struct as mount_subvolume_struct
 )
 from .btrfssubvolume import BtrfsSubvolume as BtrfsSubvolume
 from .btrfspartition import BTRFSPartition as BTRFSPartition
