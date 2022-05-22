f9fbd4698773f4bda62e7a84832d5d0f98c4e3f2
Daniel Girtler <girtler.daniel@gmail.com>
2022-05-22 12:00:08 +1000

    Make users mypy compatible

--- archinstall/lib/models/users.py
+++ archinstall/lib/models/users.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Any, TYPE_CHECKING
+
+if TYPE_CHECKING:
+	_: Any
 
 
 @dataclass
@@ -14,7 +17,7 @@ class User:
 		# if it's every going to be used
 		return []
 
-	def json(self) -> Dict[str, str]:
+	def json(self) -> Dict[str, Any]:
 		return {
 			'username': self.username,
 			'!password': self.password,
@@ -26,7 +29,7 @@ class User:
 		return f'{_("Username")}: {self.username:16} {_("Password")}: {password:16} sudo: {str(self.sudo)}'
 
 	@classmethod
-	def _parse(cls, config_users: List[Dict[str, str]]) -> List['User']:
+	def _parse(cls, config_users: List[Dict[str, Any]]) -> List['User']:
 		users = []
 
 		for entry in config_users:
@@ -43,7 +46,7 @@ class User:
 		return users
 
 	@classmethod
-	def _parse_backwards_compatible(cls, config_users: Dict[str, Dict[str, str]], sudo: bool) -> List['User']:
+	def _parse_backwards_compatible(cls, config_users: Dict, sudo: bool) -> List['User']:
 		if len(config_users.keys()) > 0:
 			username = list(config_users.keys())[0]
 			password = config_users[username]['!password']
@@ -62,13 +65,13 @@ class User:
 		users = []
 
 		# backwards compability
-		if isinstance(config_users, Dict):
+		if isinstance(config_users, dict):
 			users += cls._parse_backwards_compatible(config_users, False)
 		else:
 			users += cls._parse(config_users)
 
 		# backwards compability
-		if isinstance(config_superusers, Dict):
+		if isinstance(config_superusers, dict):
 			users += cls._parse_backwards_compatible(config_superusers, True)
 
 		return users
