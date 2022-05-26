4b4529de92a3a9b7cc0bb206553a927b411eac2c
Anton Hvornum <anton@hvornum.se>
2022-05-26 10:29:38 +0200

    Debugging

--- archinstall/lib/installer.py
+++ archinstall/lib/installer.py
@@ -319,7 +319,7 @@ class Installer:
 		# We mount everything by sorting on the mountpoint itself.
 		print(list(sorted(mount_queue.items(), key=lambda item: item[0])))
 		for mountpoint, frozen_func in sorted(mount_queue.items(), key=lambda item: item[0]):
-			print('-- MOUNTPOINT:', mountpoint)
+			print('-- MOUNTPOINT:', mountpoint, name, btrfs_subvolume_information)
 			frozen_func()
 
 			time.sleep(1)
