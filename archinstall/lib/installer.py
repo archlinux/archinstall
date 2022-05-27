fbab7fc31fd0d61515f305e65d0643b2d13f70fd
Anton Hvornum <anton@hvornum.se>
2022-05-27 21:22:55 +0200

    Debugging.

--- archinstall/lib/installer.py
+++ archinstall/lib/installer.py
@@ -314,6 +314,7 @@ class Installer:
 
 			if partition.get('filesystem',{}).get('mount_options',[]):
 				mount_options = ','.join(partition['filesystem']['mount_options'])
+				print('****', mount_options)
 				mount_queue[mountpoint] = lambda instance=partition['device_instance'], target=f"{self.target}{mountpoint}", options=mount_options: instance.mount(target, options)
 			else:
 				mount_queue[mountpoint] = lambda instance=partition['device_instance'], target=f"{self.target}{mountpoint}": instance.mount(target)
