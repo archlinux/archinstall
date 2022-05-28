2ac26752dc0c7ece2db658ede2fe1f661e2b5be8
Dylan M. Taylor <dylan@dylanmtaylor.com>
2022-05-28 12:14:50 -0400

    Ref: https://github.com/archlinux/archinstall/blob/master/archinstall/lib/storage.py

--- archinstall/lib/menu/global_menu.py
+++ archinstall/lib/menu/global_menu.py
@@ -207,7 +207,7 @@ class GlobalMenu(GeneralMenu):
 					if storage['arguments']['disk_layouts'][blockdevice].get('partitions'):
 						for partition_index in select_encrypted_partitions(
 								title=_('Select which partitions to encrypt:'),
-								partitions=list(filter(lambda p: p.mountpoint != '/boot'), storage['arguments']['disk_layouts'][blockdevice]['partitions']))
+								partitions=list(filter(lambda p: p['MOUNT_POINT'] != '/boot'), storage['arguments']['disk_layouts'][blockdevice]['partitions']))
 							):
 
 							partition = storage['arguments']['disk_layouts'][blockdevice]['partitions'][partition_index]
