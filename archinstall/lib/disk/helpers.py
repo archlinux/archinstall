79c60cfdf54af461c51cad5f939f7bc392fbf054
Anton Hvornum <anton@hvornum.se>
2022-05-28 10:01:12 +0200

    Debugging.

--- archinstall/lib/disk/helpers.py
+++ archinstall/lib/disk/helpers.py
@@ -433,7 +433,7 @@ def disk_layouts() -> Optional[Dict[str, Any]]:
 
 
 def encrypted_partitions(blockdevices :Dict[str, Any]) -> bool:
-	for blockdevice in blockdevices:
+	for blockdevice in blockdevices.values():
 		print(blockdevice)
 		for partition in blockdevice.get('partitions', []).values():
 			if partition.get('encrypted', False):
