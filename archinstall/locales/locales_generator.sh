644b80d0bd1323bdd47be5103a6ac854679ff5e0
Daniel Girtler <blackrabbit256@gmail.com>
2022-06-07 18:56:15 +1000

    Don't generate any backup locales files (#1313)
    Co-authored-by: Daniel Girtler <girtler.daniel@gmail.com>

--- archinstall/locales/locales_generator.sh
+++ archinstall/locales/locales_generator.sh
@@ -7,6 +7,6 @@ find . -type f -iname "*.py" | xargs xgettext --join-existing --no-location --om
 for file in $(find locales/ -name "base.po"); do
 	echo "Updating: $file"
 	path=$(dirname $file)
-	msgmerge --quiet --no-location --width 512 --update $file locales/base.pot
+	msgmerge --quiet --no-location --width 512 --backup none --update $file locales/base.pot
 	msgfmt -o $path/base.mo $file
 done
