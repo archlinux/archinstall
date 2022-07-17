7b44a31c91c8cacd43bf5ca1b379c9396e685a67
Samaoo <sam.verhaegen2003@gmail.com>
2022-07-17 22:22:05 +0200

    fix `valid_parted_position()`

--- archinstall/lib/disk/validators.py
+++ archinstall/lib/disk/validators.py
@@ -1,19 +1,13 @@
 from typing import List
 
 def valid_parted_position(pos :str) -> bool:
-	if not len(pos):
+	if not pos:
 		return False
 
 	if pos.isdigit():
 		return True
 
-	if pos[-1] == '%' and pos[:-1].isdigit():
-		return True
-
-	if pos[-3:].lower() in ['mib', 'kib', 'b', 'tib'] and pos[:-3].replace(".", "", 1).isdigit():
-		return True
-
-	if pos[-2:].lower() in ['kb', 'mb', 'gb', 'tb'] and pos[:-2].replace(".", "", 1).isdigit():
+	if any(pos.endswith(size) and pos[:-len(size)].replace(".", "", 1).isdigit() for size in ['%', 'b', 'kb', 'mb', 'gb', 'tb', 'kib', 'mib', 'gib', 'tib']):
 		return True
 
 	return False
