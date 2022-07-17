a8904a0041fea4db533474e9f40bb4adf0b67a87
Samaoo <sam.verhaegen2003@gmail.com>
2022-07-17 22:51:46 +0200

    change `pos` to `pos.lower()`

--- archinstall/lib/disk/validators.py
+++ archinstall/lib/disk/validators.py
@@ -7,7 +7,7 @@ def valid_parted_position(pos :str) -> bool:
 	if pos.isdigit():
 		return True
 
-	if any(pos.endswith(size) and pos[:-len(size)].replace(".", "", 1).isdigit()
+	if any(pos.lower().endswith(size) and pos[:-len(size)].replace(".", "", 1).isdigit()
 			for size in ['%', 'b', 'kb', 'mb', 'gb', 'tb', 'kib', 'mib', 'gib', 'tib']):
 		return True
 
