20dd318699d15dc73e04ac639a77c5852849b5af
Anton Hvornum <anton@hvornum.se>
2022-08-30 21:38:46 +0200

    Trying to improvePartition()._fetch_information()

--- archinstall/lib/exceptions.py
+++ archinstall/lib/exceptions.py
@@ -17,10 +17,11 @@ class ProfileError(BaseException):
 
 
 class SysCallError(BaseException):
-	def __init__(self, message :str, exit_code :Optional[int] = None) -> None:
+	def __init__(self, message :str, exit_code :Optional[int] = None, worker :Optional['SysCommandWorker'] = None) -> None:
 		super(SysCallError, self).__init__(message)
 		self.message = message
 		self.exit_code = exit_code
+		self.worker = worker
 
 
 class PermissionError(BaseException):
