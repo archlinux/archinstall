5066c60f51f5bf6acf48a6ce789cfdeda8a707df
Anton Hvornum <anton@hvornum.se>
2022-09-19 21:38:06 +0200

    Allowing the genfstab plugin to break on success

--- archinstall/lib/installer.py
+++ archinstall/lib/installer.py
@@ -432,7 +432,8 @@ class Installer:
 
 		for plugin in plugins.values():
 			if hasattr(plugin, 'on_genfstab'):
-				plugin.on_genfstab(self)
+				if plugin.on_genfstab(self) is True:
+					break
 
 		return True
 
