550d678a8d136e19772b2742ff4e40bd1581d56f
Daniel Girtler <girtler.daniel@gmail.com>
2023-03-11 09:40:38 +1100

    fix flake8

--- archinstall/lib/installer.py
+++ archinstall/lib/installer.py
@@ -319,12 +319,12 @@ class Installer:
 
 		partition.mount(f'{self.target}{mountpoint}', options=options)
 
-	def add_swapfile(self, size = '4G', enable_resume = True, file='/swapfile'):
+	def add_swapfile(self, size='4G', enable_resume=True, file='/swapfile'):
 		if file[:1] != '/':
 			file = f"/{file}"
 		if len(file.strip()) <= 0 or file == '/':
 			raise ValueError(f"The filename for the swap file has to be a valid path, not: {self.target}{file}")
-			
+
 		SysCommand(f'dd if=/dev/zero of={self.target}{file} bs={size} count=1')
 		SysCommand(f'chmod 0600 {self.target}{file}')
 		SysCommand(f'mkswap {self.target}{file}')
@@ -337,7 +337,7 @@ class Installer:
 
 			self.HOOKS.append('resume')
 			self.KERNEL_PARAMS.append(f'resume=UUID={resume_uuid}')
-			self.KERNEL_PARAMS.append(f'resume_offset={resume_offset}')			
+			self.KERNEL_PARAMS.append(f'resume_offset={resume_offset}')
 
 	def post_install_check(self, *args :str, **kwargs :str) -> List[str]:
 		return [step for step, flag in self.helper_flags.items() if flag is False]
@@ -452,12 +452,10 @@ class Installer:
 			if hasattr(plugin, 'on_genfstab'):
 				if plugin.on_genfstab(self) is True:
 					break
-		
+
 		with open(f"{self.target}/etc/fstab", 'a') as fstab_fh:
 			for entry in self.FSTAB_ENTRIES:
 				fstab_fh.write(f'{entry}\n')
-
-
 		return True
 
 	def set_hostname(self, hostname: str, *args :str, **kwargs :str) -> None:
