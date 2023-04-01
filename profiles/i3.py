58db762641876d6e2028628a8a6e28affadd4e6b
r0t0r-r0t0r <soap4me@gmail.com>
2023-04-01 21:14:05 +0300

    Fix i3 profile

--- profiles/i3.py
+++ profiles/i3.py
@@ -7,7 +7,7 @@ is_top_level_profile = False
 # New way of defining packages for a profile, which is iterable and can be used out side
 # of the profile to get a list of "what packages will be installed".
 __packages__ = [
-	'i3-wm'
+	'i3-wm',
 	'i3lock',
 	'i3status',
 	'i3blocks',
