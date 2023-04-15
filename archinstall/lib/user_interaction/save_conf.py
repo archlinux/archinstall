5dc72bdfc6d1ad6ca4aaf67c40efaa7f9236adc3
Daniel Girtler <girtler.daniel@gmail.com>
2023-04-15 18:00:29 +1000

    Fix saving config

--- archinstall/lib/user_interaction/save_conf.py
+++ archinstall/lib/user_interaction/save_conf.py
@@ -5,16 +5,17 @@ import logging
 from pathlib import Path
 from typing import Any, Dict, TYPE_CHECKING
 
-from ..configuration import ConfigurationOutput
-from ..menu import Menu, MenuSelectionType
+from ..general import SysCommand
+from ..menu import Menu
+from ..menu.menu import MenuSelectionType
 from ..output import log
+from ..configuration import ConfigurationOutput
 
 if TYPE_CHECKING:
 	_: Any
 
 
 def save_config(config: Dict):
-
 	def preview(selection: str):
 		if options['user_config'] == selection:
 			serialized = config_output.user_config_to_json()
@@ -36,6 +37,7 @@ def save_config(config: Dict):
 	options = {
 		'user_config': str(_('Save user configuration')),
 		'user_creds': str(_('Save user credentials')),
+		'disk_layout': str(_('Save disk layout')),
 		'all': str(_('Save all'))
 	}
 
@@ -51,61 +53,53 @@ def save_config(config: Dict):
 	if choice.type_ == MenuSelectionType.Skip:
 		return
 
-	save_config_value: str = choice.single_value
+	save_config_value = choice.single_value
+	saving_key = [k for k, v in options.items() if v == save_config_value][0]
 
-	# dirs_to_exclude = [
-	# 	'/bin',
-	# 	'/dev',
-	# 	'/lib',
-	# 	'/lib64',
-	# 	'/lost+found',
-	# 	'/opt',
-	# 	'/proc',
-	# 	'/run',
-	# 	'/sbin',
-	# 	'/srv',
-	# 	'/sys',
-	# 	'/usr',
-	# 	'/var',
-	# ]
+	dirs_to_exclude = [
+		'/bin',
+		'/dev',
+		'/lib',
+		'/lib64',
+		'/lost+found',
+		'/opt',
+		'/proc',
+		'/run',
+		'/sbin',
+		'/srv',
+		'/sys',
+		'/usr',
+		'/var',
+	]
 
-	# log('When picking a directory to save configuration files to, by default we will ignore the following folders: ' + ','.join(dirs_to_exclude), level=logging.DEBUG)
+	log('Ignore configuration option folders: ' + ','.join(dirs_to_exclude), level=logging.DEBUG)
+	log(_('Finding possible directories to save configuration files ...'), level=logging.INFO)
 
-	# log(_('Finding possible directories to save configuration files ...'), level=logging.INFO)
-	#
-	# find_exclude = '-path ' + ' -prune -o -path '.join(dirs_to_exclude) + ' -prune '
-	# file_picker_command = f'find / {find_exclude} -o -type d -print0'
-	# decoded = SysCommand(file_picker_command).decode()
-	#
-	# if not decoded:
-	# 	raise ValueError(f'Error decoding command result: {file_picker_command}')
-	#
-	# possible_save_dirs = list(filter(None, decoded.split('\x00')))
-	#
-	# choice = Menu(
-	# 	_('Select directory (or directories) for saving configuration files'),
-	# 	possible_save_dirs,
-	# 	multi=True,
-	# 	skip=True,
-	# 	allow_reset=False,
-	# ).run()
+	find_exclude = '-path ' + ' -prune -o -path '.join(dirs_to_exclude) + ' -prune '
+	file_picker_command = f'find / {find_exclude} -o -type d -print0'
 
-	# if choice.type_ == MenuSelectionType.Skip:
-	# 	return
+	directories = SysCommand(file_picker_command).decode()
 
-	save_dirs = choice.single_value
-	save_dirs = ['/tmp']
+	if directories is None:
+		raise ValueError('Failed to retrieve possible configuration directories')
 
-	# prompt = _('Do you want to save {} configuration file(s) in the following locations?\n\n{}').format(
-	# 	save_config_value,
-	# 	', '.join(save_dirs)
-	# )
+	possible_save_dirs = list(filter(None, directories.split('\x00')))
 
-	# save_confirmation = Menu(prompt, Menu.yes_no(), default_option=Menu.yes()).run()
-	# if save_confirmation == Menu.no():
-	# 	return
+	selection = Menu(
+		_('Select directory (or directories) for saving configuration files'),
+		possible_save_dirs,
+		multi=True,
+		skip=True,
+		allow_reset=False,
+	).run()
 
-	log('Saving {} configuration files to {}'.format(save_config_value, save_dirs), level=logging.DEBUG)
+	match selection.type_:
+		case MenuSelectionType.Skip:
+			return
+
+	save_dirs = selection.multi_value
+
+	log(f'Saving {saving_key} configuration files to {save_dirs}', level=logging.DEBUG)
 
 	if save_dirs is not None:
 		for save_dir_str in save_dirs:
