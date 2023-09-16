c9e2027725a75f08cee51e1fe02af93650d9c340
codefiles <11915375+codefiles@users.noreply.github.com>
2023-09-16 17:30:46 -0400

    Fix encryption debug message in `_add_efistub_bootloader()` (#1991)

--- archinstall/lib/installer.py
+++ archinstall/lib/installer.py
@@ -1073,10 +1073,10 @@ TIMEOUT=5
 			if root_partition in self._disk_encryption.partitions:
 				# TODO: We need to detect if the encrypted device is a whole disk encryption,
 				#       or simply a partition encryption. Right now we assume it's a partition (and we always have)
-				debug(f'Identifying root partition by PARTUUID: {root_partition.partuuid}')
+				debug(f'Root partition is an encrypted device identifying by PARTUUID: {root_partition.partuuid}')
 				kernel_parameters.append(f'cryptdevice=PARTUUID={root_partition.partuuid}:luksdev root=/dev/mapper/luksdev rw rootfstype={root_partition.safe_fs_type.value} {" ".join(self._kernel_params)}')
 			else:
-				debug(f'Root partition is an encrypted device identifying by PARTUUID: {root_partition.partuuid}')
+				debug(f'Identifying root partition by PARTUUID: {root_partition.partuuid}')
 				kernel_parameters.append(f'root=PARTUUID={root_partition.partuuid} rw rootfstype={root_partition.safe_fs_type.value} {" ".join(self._kernel_params)}')
 
 			parent_dev_path = disk.device_handler.get_parent_device_path(boot_partition.safe_dev_path)
