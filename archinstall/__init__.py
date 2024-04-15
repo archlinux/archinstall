7d9e9d8ba0bcba888ec46554f87dfc414c73f9c4
Anton Hvornum <anton@hvornum.se>
2024-04-15 06:50:17 +0200

    Bumping version to: v2.8.0 (#2460)

--- archinstall/__init__.py
+++ archinstall/__init__.py
@@ -42,7 +42,7 @@ if TYPE_CHECKING:
 	_: Any
 
 
-__version__ = "2.7.2"
+__version__ = "2.8.0"
 storage['__version__'] = __version__
 
 # add the custom _ as a builtin, it can now be used anywhere in the
