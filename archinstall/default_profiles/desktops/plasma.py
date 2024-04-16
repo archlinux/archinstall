0a17460d384f23ff972371d79ddf86cdc7954212
Martin Rys <martin@rys.pw>
2024-04-16 14:25:11 +0200

    Rename to KDE Plasma in user facing parts and keep the old "Kde" profile for now

--- archinstall/default_profiles/desktops/plasma.py
+++ archinstall/default_profiles/desktops/plasma.py
@@ -6,10 +6,30 @@ from archinstall.default_profiles.xorg import XorgProfile
 if TYPE_CHECKING:
 	_: Any
 
-
 class PlasmaProfile(XorgProfile):
 	def __init__(self):
-		super().__init__('Plasma', ProfileType.DesktopEnv, description='')
+		super().__init__('KDE Plasma', ProfileType.DesktopEnv, description='')
+
+	@property
+	def packages(self) -> List[str]:
+		return [
+			"plasma-meta",
+			"konsole",
+			"kwrite",
+			"dolphin",
+			"ark",
+			"plasma-workspace",
+			"egl-wayland"
+		]
+
+	@property
+	def default_greeter_type(self) -> Optional[GreeterType]:
+		return GreeterType.Sddm
+
+# 2024-04-16 TODO deprecated Class with old naming, remove in a future version
+class KdeProfile(XorgProfile):
+	def __init__(self):
+		super().__init__('Kde', ProfileType.DesktopEnv, description='[Deprecated] Alias to KDE Plasma')
 
 	@property
 	def packages(self) -> List[str]:
