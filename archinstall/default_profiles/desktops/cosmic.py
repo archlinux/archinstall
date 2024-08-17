b0f2e446398d17f39fe8079f1cd6ff970a0bd86a
killertofus <cinemassacres@gmail.com>
2024-08-17 00:30:38 +0000

    cosmic is out of the aur

---
+++ archinstall/default_profiles/desktops/cosmic.py
@@ -0,0 +1,21 @@
+from typing import List, Optional, Any, TYPE_CHECKING
+
+from archinstall.default_profiles.profile import ProfileType, GreeterType
+from archinstall.default_profiles.xorg import XorgProfile
+
+if TYPE_CHECKING:
+	_: Any
+
+class PlasmaProfile(XorgProfile):
+	def __init__(self):
+		super().__init__('cosmic-epoch', ProfileType.DesktopEnv, description='')
+
+	@property
+	def packages(self) -> List[str]:
+		return [
+			"cosmic",
+		]
+
+	@property
+	def default_greeter_type(self) -> Optional[GreeterType]:
+		return GreeterType.cosmic-session
