2b3f6b0a07810e999667d18eeffc8a84dc583943
Daniel Girtler <girtler.daniel@gmail.com>
2024-11-19 20:42:10 +1100

    Fix seat-access selection

---
+++ archinstall/default_profiles/desktops/__init__.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class SeatAccess(Enum):
+	seatd = 'seatd'
+	polkit = 'polkit'
--- archinstall/default_profiles/desktops/sway.py
+++ archinstall/default_profiles/desktops/sway.py
@@ -1,6 +1,6 @@
-from enum import Enum
 from typing import TYPE_CHECKING, Any
 
+from archinstall.default_profiles.desktops import SeatAccess
 from archinstall.default_profiles.profile import GreeterType, ProfileType, SelectResult
 from archinstall.default_profiles.xorg import XorgProfile
 from archinstall.tui import Alignment, FrameProperties, MenuItem, MenuItemGroup, ResultType, SelectMenu
@@ -8,11 +8,6 @@ from archinstall.tui import Alignment, FrameProperties, MenuItem, MenuItemGroup,
 if TYPE_CHECKING:
 	from archinstall.lib.installer import Installer
 	_: Any
-
-
-class SeatAccess(Enum):
-	seatd = 'seatd'
-	polkit = 'polkit'
 
 
 class SwayProfile(XorgProfile):
@@ -77,7 +72,7 @@ class SwayProfile(XorgProfile):
 
 		if result.type_ == ResultType.Selection:
 			if result.item() is not None:
-				self.custom_settings['seat_access'] = result.get_value()
+				self.custom_settings['seat_access'] = result.get_value().value
 
 	def do_on_select(self) -> SelectResult | None:
 		self._ask_seat_access()
