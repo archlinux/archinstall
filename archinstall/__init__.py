aa80f8cf412d7bec1845ccc7f5678d5acccc1800
Anton Hvornum <anton@hvornum.se>
2024-11-23 11:45:32 +0100

    Bump to version 3.0.1 (#2920)

--- archinstall/__init__.py
+++ archinstall/__init__.py
@@ -39,7 +39,7 @@ if TYPE_CHECKING:
 	_: Any
 
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 storage['__version__'] = __version__
 
 # add the custom _ as a builtin, it can now be used anywhere in the
