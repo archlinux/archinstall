11f8490b5927cb62f79a18217313c17ca2df0f59
correctmost <134317971+correctmost@users.noreply.github.com>
2024-11-30 06:54:53 -0500

    Enable the bad-indentation pylint rule and fix space indentations (#2968)

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,9 @@ import pytest
 
 @pytest.fixture(scope='session')
 def config_fixture() -> Path:
-    return Path(__file__).parent / 'data' / 'test_config.json'
+	return Path(__file__).parent / 'data' / 'test_config.json'
 
 
 @pytest.fixture(scope='session')
 def creds_fixture() -> Path:
-    return Path(__file__).parent / 'data' / 'test_creds.json'
+	return Path(__file__).parent / 'data' / 'test_creds.json'
