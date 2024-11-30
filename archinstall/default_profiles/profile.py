6a6642a9c1e1a684524a43d99fe2099e8e00d8ad
correctmost <134317971+correctmost@users.noreply.github.com>
2024-11-30 17:17:10 -0500

    Replace some Any instances with specific type hints (#2973)

--- archinstall/default_profiles/profile.py
+++ archinstall/default_profiles/profile.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import sys
 from enum import Enum, auto
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 from ..lib.storage import storage
 
@@ -67,7 +67,7 @@ class Profile:
 		self.name = name
 		self.description = description
 		self.profile_type = profile_type
-		self.custom_settings: dict[str, Any] = {}
+		self.custom_settings: dict[str, str | None] = {}
 		self.advanced = advanced
 
 		self._support_gfx_driver = support_gfx_driver
@@ -124,7 +124,7 @@ class Profile:
 		are needed
 		"""
 
-	def json(self) -> dict[str, Any]:
+	def json(self) -> dict[str, str]:
 		"""
 		Returns a json representation of the profile
 		"""
@@ -136,7 +136,7 @@ class Profile:
 		"""
 		return SelectResult.NewSelection
 
-	def set_custom_settings(self, settings: dict[str, Any]) -> None:
+	def set_custom_settings(self, settings: dict[str, str | None]) -> None:
 		"""
 		Set the custom settings for the profile.
 		This is also called when the settings are parsed from the config
@@ -185,7 +185,7 @@ class Profile:
 	def is_greeter_supported(self) -> bool:
 		return self._support_greeter
 
-	def preview_text(self) -> str | None:
+	def preview_text(self) -> str:
 		"""
 		Override this method to provide a preview text for the profile
 		"""
