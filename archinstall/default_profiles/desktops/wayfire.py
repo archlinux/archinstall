7939b2be670439700c0e876673e8b52544203039
Mohamed-elg <mohamed.elguermat9@gmail.com>
2024-12-17 20:02:48 +0100

    fix pylint

--- archinstall/default_profiles/desktops/wayfire.py
+++ archinstall/default_profiles/desktops/wayfire.py
@@ -5,15 +5,19 @@ from archinstall.default_profiles.xorg import XorgProfile
 
 
 class WayfireProfile(XorgProfile):
-    def __init__(self) -> None:
-        super().__init__("wayfire", ProfileType.WindowMgr, description="Wayfire is a 3D Wayland compositor")
+	def __init__(self) -> None:
+		super().__init__(
+			"Wayfire",
+			ProfileType.WindowMgr,
+			description="Wayfire is a 3D Wayland compositor",
+		)
 
-    @property
-    @override
-    def packages(self) -> list[str]:
-        return ["wayfire", "wayfire-plugins-extra", "wf-shell", "wcm"]
+	@property
+	@override
+	def packages(self) -> list[str]:
+		return ["wayfire", "wayfire-plugins-extra", "wf-shell", "wcm"]
 
-    @property
-    @override
-    def default_greeter_type(self) -> GreeterType | None:
-        return GreeterType.Lightdm
+	@property
+	@override
+	def default_greeter_type(self) -> GreeterType | None:
+		return GreeterType.Lightdm
