22b410d082e1dc0f9bd1a10a567bfdc5ab626b99
codefiles <11915375+codefiles@users.noreply.github.com>
2025-01-10 23:42:50 -0500

    Change to import Path for consistency (#3101)

--- archinstall/scripts/list.py
+++ archinstall/scripts/list.py
@@ -1,9 +1,9 @@
 import glob
-import pathlib
+from pathlib import Path
 
 print("The following are viable --script options:")
 
-for script in [pathlib.Path(x) for x in glob.glob(f"{pathlib.Path(__file__).parent}/*.py")]:
+for script in [Path(x) for x in glob.glob(f"{Path(__file__).parent}/*.py")]:
 	if script.stem in ['__init__', 'list']:
 		continue
 
