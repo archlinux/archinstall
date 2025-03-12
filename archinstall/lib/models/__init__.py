426e6a059e0f0b09a75aa9577ed788c5b701290e
Daniel Girtler <girtler.daniel@gmail.com>
2025-03-12 21:48:54 +1100

    Refactor code

--- archinstall/lib/models/__init__.py
+++ archinstall/lib/models/__init__.py
@@ -27,9 +27,9 @@ from .device_model import (
 	Unit,
 	_DeviceInfo,
 )
-from .gen import LocalPackage, PackageSearch, PackageSearchResult, Repository
 from .locale import LocaleConfiguration
 from .mirrors import CustomRepository, MirrorConfiguration, MirrorRegion
 from .network_configuration import NetworkConfiguration, Nic, NicType
+from .packages import LocalPackage, PackageSearch, PackageSearchResult, Repository
 from .profile_model import ProfileConfiguration
 from .users import PasswordStrength, User
