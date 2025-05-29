bfb408257f211756af3b517360ea9a5a540cbafe
Daniel Girtler <girtler.daniel@gmail.com>
2025-05-29 21:24:53 +1000

    Update

--- archinstall/lib/plugins.py
+++ archinstall/lib/plugins.py
@@ -7,7 +7,6 @@ import urllib.request
 from importlib import metadata
 from pathlib import Path
 
-from .args import arch_config_handler
 from .output import error, info, warn
 
 plugins = {}
@@ -89,6 +88,8 @@ def load_plugin(path: Path) -> None:
 		namespace = _import_via_path(localized)
 
 	if namespace and namespace in sys.modules:
+		from .args import arch_config_handler
+
 		# Version dependency via __archinstall__version__ variable (if present) in the plugin
 		# Any errors in version inconsistency will be handled through normal error handling if not defined.
 		version = arch_config_handler.config.version
