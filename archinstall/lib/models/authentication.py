263ea2d6dd38fbc59c72f54eb318679cd066a67e
Daniel Girtler <girtler.daniel@gmail.com>
2025-06-29 18:47:23 +1000

    Update

--- archinstall/lib/models/authentication.py
+++ archinstall/lib/models/authentication.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any, NotRequired, TypedDict
+from typing import Any, NotRequired, Optional, TypedDict
 
 from archinstall.lib.translationhandler import tr
 
@@ -40,7 +40,7 @@ class U2FLoginConfiguration:
 		}
 
 	@staticmethod
-	def parse_arg(args: dict[str, Any]) -> 'U2FLoginConfiguration' | None:
+	def parse_arg(args: dict[str, Any]) -> Optional['U2FLoginConfiguration']:
 		u2f_login_method = args.get('u2f_login_method')
 
 		if u2f_login_method is None:
