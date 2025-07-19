f0392e810c993822c6b740a5f3f50802d94cb74f
Torxed <torxed@archlinux.org>
2025-07-19 16:08:06 +0200

    Fixed ruff

--- archinstall/lib/models/bootloader.py
+++ archinstall/lib/models/bootloader.py
@@ -29,6 +29,10 @@ class Bootloader(Enum):
 
 	@classmethod
 	def get_default(cls) -> None | Bootloader:
+		from ..args import arch_config_handler
+
+		if arch_config_handler.args.skip_boot:
+			return None
 		elif SysInfo.has_uefi():
 			return Bootloader.Systemd
 		else:
