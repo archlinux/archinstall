b77a43b20113cf02f651f79bde5437bbe3d64e35
correctmost <134317971+correctmost@users.noreply.github.com>
2025-07-23 14:00:21 -0400

    Remove unneeded None annotation from Bootloader.get_default (#3680)

--- archinstall/lib/models/bootloader.py
+++ archinstall/lib/models/bootloader.py
@@ -31,7 +31,7 @@ class Bootloader(Enum):
 		return [e.value for e in Bootloader if e != Bootloader.NO_BOOTLOADER or arch_config_handler.args.skip_boot is True]
 
 	@classmethod
-	def get_default(cls) -> None | Bootloader:
+	def get_default(cls) -> Bootloader:
 		from ..args import arch_config_handler
 
 		if arch_config_handler.args.skip_boot:
