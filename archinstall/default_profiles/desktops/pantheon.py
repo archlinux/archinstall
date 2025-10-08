de9f725808ccf8c9c8e1cb04b3cd4801416acc11
P-rth <67070874+P-rth@users.noreply.github.com>
2025-10-08 14:25:38 +0530

    fix linting
    replaced all spaces with tabs

--- archinstall/default_profiles/desktops/pantheon.py
+++ archinstall/default_profiles/desktops/pantheon.py
@@ -6,29 +6,29 @@ from archinstall.default_profiles.xorg import XorgProfile
 
 class PantheonProfile(XorgProfile):
 
-    def __init__(self) -> None:
-        super().__init__('Pantheon', ProfileType.DesktopEnv)
+	def __init__(self) -> None:
+		super().__init__('Pantheon', ProfileType.DesktopEnv)
 
-    @property
-    @override
-    def packages(self) -> list[str]:
-        return [
-            'pantheon-session',
-            'pantheon-polkit-agent',
-            'pantheon-print',
-            'pantheon-settings-daemon',
-            'sound-theme-elementary',
-            'switchboard',
-            'switchboard-plug-desktop',
-            'elementary-icon-theme',
-            'wingpanel-indicator-session',
-            'wingpanel-indicator-datetime',
-            'pantheon-applications-menu',
-            'gnome-settings-daemon',
-            'pantheon-default-settings',
-        ]
+	@property
+	@override
+	def packages(self) -> list[str]:
+		return [
+			'pantheon-session',
+			'pantheon-polkit-agent',
+			'pantheon-print',
+			'pantheon-settings-daemon',
+			'sound-theme-elementary',
+			'switchboard',
+			'switchboard-plug-desktop',
+			'elementary-icon-theme',
+			'wingpanel-indicator-session',
+			'wingpanel-indicator-datetime',
+			'pantheon-applications-menu',
+			'gnome-settings-daemon',
+			'pantheon-default-settings',
+		]
 
-    @property
-    @override
-    def default_greeter_type(self) -> GreeterType:
-        return GreeterType.LightdmSlick
+	@property
+	@override
+	def default_greeter_type(self) -> GreeterType:
+		return GreeterType.LightdmSlick
