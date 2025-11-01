1174800cca791534318c7dd335cc1b471988f508
Klaus Zipfel <30482165+systemofapwne@users.noreply.github.com>
2025-11-01 11:59:24 +0100

    Skip bootloader config check when 'No Bootloader' is selected as bootloader (#3875)
    * Allow installation via TUI when 'No Bootloader' is selected as bootloader (--skip-boot)
    
    * Update global_menu.py
    
    Fixed ruff formatting issue
    
    ---------
    
    Co-authored-by: Anton Hvornum <anton.feeds+github@gmail.com>

--- archinstall/lib/global_menu.py
+++ archinstall/lib/global_menu.py
@@ -425,6 +425,9 @@ class GlobalMenu(AbstractMenu[None]):
 
 		bootloader = self._item_group.find_by_key('bootloader').value
 
+		if bootloader == Bootloader.NO_BOOTLOADER:
+			return None
+
 		if disk_config := self._item_group.find_by_key('disk_config').value:
 			for layout in disk_config.device_modifications:
 				if root_partition := layout.get_root_partition():
