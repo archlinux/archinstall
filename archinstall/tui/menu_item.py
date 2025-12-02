df0231ea32a2f5661173be52575bf48115f95218
Gabeahz <gabeahz@gmail.com>
2025-12-02 13:52:01 -0800

    Menu now filters and sorts using priority, improving UX.

--- archinstall/tui/menu_item.py
+++ archinstall/tui/menu_item.py
@@ -231,8 +231,17 @@ class MenuItemGroup:
 	def items(self) -> list[MenuItem]:
 		pattern = self._filter_pattern.lower()
 		items = filter(lambda item: item.is_empty() or pattern in item.text.lower(), self._menu_items)
-		l_items = list(items)
+		l_items = sorted(items, key=self._items_score)
 		return l_items
+
+	def _items_score(self, item: MenuItem) -> int:
+		pattern = self._filter_pattern.lower()
+		if pattern in item.text.lower():
+			if item.text.lower().startswith(pattern):
+				return 0
+			else:
+				return 1
+		return 2
 
 	@property
 	def filter_pattern(self) -> str:
