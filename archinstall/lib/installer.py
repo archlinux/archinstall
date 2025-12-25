9e7a5f693108aad80a731bb719454547b97e50c9
HADEON <52324046+h8d13@users.noreply.github.com>
2025-12-25 01:00:34 +0100

    Do not install base-devel by default (#4022)
    * replace base-devel with sudo and add disclaimer texts
    
    * revert basepot and add rem comments

--- archinstall/lib/installer.py
+++ archinstall/lib/installer.py
@@ -52,7 +52,7 @@ from .plugins import plugins
 from .storage import storage
 
 # Any package that the Installer() is responsible for (optional and the default ones)
-__packages__ = ['base', 'base-devel', 'linux-firmware', 'linux', 'linux-lts', 'linux-zen', 'linux-hardened']
+__packages__ = ['base', 'sudo', 'linux-firmware', 'linux', 'linux-lts', 'linux-zen', 'linux-hardened']
 
 # Additional packages that are installed if the user is running the Live ISO with accessibility tools enabled
 __accessibility_packages__ = ['brltty', 'espeakup', 'alsa-utils']
--- archinstall/lib/interactions/general_conf.py
+++ archinstall/lib/interactions/general_conf.py
@@ -161,7 +161,8 @@ def ask_additional_packages_to_install(
 	package_groups = PackageGroup.from_available_packages(packages)
 
 	# Additional packages (with some light weight error handling for invalid package names)
-	header = tr('Only packages such as base, base-devel, linux, linux-firmware, efibootmgr and optional profile packages are installed.') + '\n'
+	header = tr('Only packages such as base, sudo, linux, linux-firmware, efibootmgr and optional profile packages are installed.') + '\n'
+	header += tr('Note: base-devel is no longer installed by default. Add it here if you need build tools.') + '\n'
 	header += tr('Select any packages from the below list that should be installed additionally') + '\n'
 
 	# there are over 15k packages so this needs to be quick
