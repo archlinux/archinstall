af6b817a8d2a26066305fc495cf6ca1d404b6f36
h8d13 <hadean-eon-dev@proton.me>
2026-01-02 12:57:28 +0100

    Ruff

--- archinstall/lib/installer.py
+++ archinstall/lib/installer.py
@@ -886,23 +886,19 @@ class Installer:
 			self._base_packages.append('bcachefs-dkms')
 
 		# https://github.com/archlinux/archinstall/issues/1837
-  		# https://github.com/koverstreet/bcachefs/issues/916
+		# https://github.com/koverstreet/bcachefs/issues/916
 		if fs_type.fs_type_mount in ('btrfs', 'bcachefs'):
-            self._disable_fstrim = True
-        
-        if fs_type.fs_type_mount == 'bcachefs':
-            # This line (894) is the one causing the current error. 
-            # It needs 12 spaces of indentation.
-            if 'bcachefs' not in self._modules:
-                self._modules.append('bcachefs')
+			self._disable_fstrim = True
 
-            if 'bcachefs' not in self._hooks:
-                try:
-                    block_index = self._hooks.index('block')
-                    self._hooks.insert(block_index + 1, 'bcachefs')    
-                except ValueError:
-                    # Added a pass here to properly close the try/except block.
-                    pass
+		if fs_type.fs_type_mount == 'bcachefs' and 'bcachefs' not in self._modules:
+			self._modules.append('bcachefs')
+
+		if 'bcachefs' not in self._hooks:
+			try:
+				block_index = self._hooks.index('block')
+				self._hooks.insert(block_index + 1, 'bcachefs')
+			except ValueError:
+				pass
 
 		# There is not yet an fsck tool for NTFS. If it's being used for the root filesystem, the hook should be removed.
 		if fs_type.fs_type_mount == 'ntfs3' and mountpoint == self.target:
@@ -1238,7 +1234,7 @@ class Installer:
 				if sub_vol.is_root():
 					kernel_parameters.append(f'rootflags=subvol={sub_vol.name}')
 					break
-		
+
 			kernel_parameters.append('rw')
 
 		kernel_parameters.append(f'rootfstype={root.safe_fs_type.fs_type_mount}')
