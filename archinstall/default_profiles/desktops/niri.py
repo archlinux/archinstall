8f5988f983298aa72f298c9a4758b008e56ac181
h8d13 <hadrien.loge@gmail.com>
2026-01-09 12:59:52 +0100

    niri has seatd in hard deps

--- archinstall/default_profiles/desktops/niri.py
+++ archinstall/default_profiles/desktops/niri.py
@@ -17,8 +17,6 @@ class NiriProfile(WaylandProfile):
 			ProfileType.WindowMgr,
 		)
 
-		self.custom_settings = {'seat_access': None}
-
 	@property
 	@override
 	def packages(self) -> list[str]:
@@ -43,37 +41,3 @@ class NiriProfile(WaylandProfile):
 	@override
 	def default_greeter_type(self) -> GreeterType:
 		return GreeterType.Lightdm
-
-	@property
-	@override
-	def services(self) -> list[str]:
-		if pref := self.custom_settings.get('seat_access', None):
-			return [pref]
-		return []
-
-	def _ask_seat_access(self) -> None:
-		# need to activate seat service and add to seat group
-		header = tr('niri needs access to your seat (collection of hardware devices i.e. keyboard, mouse, etc)')
-		header += '\n' + tr('Choose an option to give niri access to your hardware') + '\n'
-
-		items = [MenuItem(s.value, value=s) for s in SeatAccess]
-		group = MenuItemGroup(items, sort_items=True)
-
-		default = self.custom_settings.get('seat_access', None)
-		group.set_default_by_value(default)
-
-		result = SelectMenu[SeatAccess](
-			group,
-			header=header,
-			allow_skip=False,
-			frame=FrameProperties.min(tr('Seat access')),
-			alignment=Alignment.CENTER,
-		).run()
-
-		if result.type_ == ResultType.Selection:
-			self.custom_settings['seat_access'] = result.get_value().value
-
-	@override
-	def do_on_select(self) -> None:
-		self._ask_seat_access()
-		return None
