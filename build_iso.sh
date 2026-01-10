7df8ce62b23f4da25e738d49ef7b75289cde83be
h8d13 <hadrien.loge@gmail.com>
2026-01-10 10:53:37 +0100

    Init

--- build_iso.sh
+++ build_iso.sh
@@ -1,53 +1,53 @@
-#!/bin/bash
+# #!/bin/bash
 
-set -e
+# set -e
 
-packages_file="/tmp/archlive/packages.x86_64"
+# packages_file="/tmp/archlive/packages.x86_64"
 
-# Packages to add to the archiso profile packages
-packages=(
-	gcc
-	git
-	pkgconfig
-	python
-	python-pip
-	python-uv
-	python-setuptools
-	python-pyparted
-	python-pydantic
-	python-textual
-)
+# # Packages to add to the archiso profile packages
+# packages=(
+# 	gcc
+# 	git
+# 	pkgconfig
+# 	python
+# 	python-pip
+# 	python-uv
+# 	python-setuptools
+# 	python-pyparted
+# 	python-pydantic
+# 	python-textual
+# )
 
-if [ -d "/tmp/archlive" ]; then
-    rm -rf "/tmp/archlive"
-fi
-mkdir -p /tmp/archlive/airootfs/root/archinstall-git
-cp -r . /tmp/archlive/airootfs/root/archinstall-git
+# if [ -d "/tmp/archlive" ]; then
+#     rm -rf "/tmp/archlive"
+# fi
+# mkdir -p /tmp/archlive/airootfs/root/archinstall-git
+# cp -r . /tmp/archlive/airootfs/root/archinstall-git
 
-cat <<- _EOF_ | tee /tmp/archlive/airootfs/root/.zprofile
-	cd archinstall-git
-	rm -rf dist
+# cat <<- _EOF_ | tee /tmp/archlive/airootfs/root/.zprofile
+# 	cd archinstall-git
+# 	rm -rf dist
 
-	uv build --no-build-isolation --wheel
-	uv pip install dist/*.whl --break-system-packages --system --no-build --no-deps
+# 	uv build --no-build-isolation --wheel
+# 	uv pip install dist/*.whl --break-system-packages --system --no-build --no-deps
 
-	echo "This is an unofficial ISO for development and testing of archinstall. No support will be provided."
-	echo "This ISO was built from Git SHA $GITHUB_SHA"
-	echo "Type archinstall to launch the installer."
-_EOF_
+# 	echo "This is an unofficial ISO for development and testing of archinstall. No support will be provided."
+# 	echo "This ISO was built from Git SHA $GITHUB_SHA"
+# 	echo "Type archinstall to launch the installer."
+# _EOF_
 
-pacman --noconfirm -S archiso
+# pacman --noconfirm -S archiso
 
-cp -r /usr/share/archiso/configs/releng/* /tmp/archlive
+# cp -r /usr/share/archiso/configs/releng/* /tmp/archlive
 
-sed -i /archinstall/d "$packages_file"
+# sed -i /archinstall/d "$packages_file"
 
-# Add packages to the archiso profile packages
-for package in "${packages[@]}"; do
-	echo "$package" >> "$packages_file"
-done
+# # Add packages to the archiso profile packages
+# for package in "${packages[@]}"; do
+# 	echo "$package" >> "$packages_file"
+# done
 
-find /tmp/archlive
-cd /tmp/archlive
+# find /tmp/archlive
+# cd /tmp/archlive
 
-mkarchiso -v -w work/ -o out/ ./
+# mkarchiso -v -w work/ -o out/ ./
