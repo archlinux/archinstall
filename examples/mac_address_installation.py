bb4bcf54237c4b8f6601caa609ba9a42617503c2
Daniel Girtler <dgirtler@atlassian.com>
2026-01-14 19:52:03 +1100

    Reinstate example

---
+++ examples/mac_address_installation.py
@@ -0,0 +1,19 @@
+import time
+
+from archinstall.lib.output import info
+from archinstall.lib.profile.profiles_handler import profile_handler
+from archinstall.lib.storage import storage
+
+for _profile in profile_handler.get_mac_addr_profiles():
+	# Tailored means it's a match for this machine
+	# based on its MAC address (or some other criteria
+	# that fits the requirements for this machine specifically).
+	info(f'Found a tailored profile for this machine called: "{_profile.name}"')
+
+	print('Starting install in:')
+	for i in range(10, 0, -1):
+		print(f'{i}...')
+		time.sleep(1)
+
+	install_session = storage['installation_session']
+	_profile.install(install_session)
