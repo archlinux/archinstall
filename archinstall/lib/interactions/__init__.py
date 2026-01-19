5ba200cf20665a04c21eb591f0852f472435684f
Daniel Girtler <girtler.daniel@gmail.com>
2026-01-19 22:02:57 +1100

    Move network menu

--- archinstall/lib/interactions/__init__.py
+++ archinstall/lib/interactions/__init__.py
@@ -15,11 +15,9 @@ from .general_conf import (
 	select_archinstall_language,
 )
 from .manage_users_conf import UserList, ask_for_additional_users
-from .network_menu import ManualNetworkConfig, ask_to_configure_network
 from .system_conf import ask_for_swap, select_driver, select_kernel
 
 __all__ = [
-	'ManualNetworkConfig',
 	'UserList',
 	'add_number_of_parallel_downloads',
 	'ask_additional_packages_to_install',
@@ -28,7 +26,6 @@ __all__ = [
 	'ask_for_swap',
 	'ask_hostname',
 	'ask_ntp',
-	'ask_to_configure_network',
 	'get_default_partition_layout',
 	'select_archinstall_language',
 	'select_devices',
