8e34303175bc05b7a03c4b5be07949b7adf444de
Daniel Girtler <girtler.daniel@gmail.com>
2026-03-21 17:56:41 +1100

    Fix iwd network installation and setup (#4271)
    * Fix 4223 - IWD
    
    * Update
    
    * Update

--- archinstall/lib/network/network_handler.py
+++ archinstall/lib/network/network_handler.py
@@ -3,40 +3,45 @@ from archinstall.lib.models.network import NetworkConfiguration, NicType
 from archinstall.lib.models.profile import ProfileConfiguration
 
 
-class NetworkHandler:
-	def install_network_config(
-		self,
-		network_config: NetworkConfiguration,
-		installation: Installer,
-		profile_config: ProfileConfiguration | None = None,
-	) -> None:
-		match network_config.type:
-			case NicType.ISO:
-				_ = installation.copy_iso_network_config(
-					enable_services=True,  # Sources the ISO network configuration to the install medium.
-				)
-			case NicType.NM | NicType.NM_IWD:
-				# Install NetworkManager package for both cases
-				packages = ['networkmanager']
-				# Default back-end only for non-iwd
-				if network_config.type == NicType.NM:
-					packages.append('wpa_supplicant')
+def install_network_config(
+	network_config: NetworkConfiguration,
+	installation: Installer,
+	profile_config: ProfileConfiguration | None = None,
+) -> None:
+	match network_config.type:
+		case NicType.ISO:
+			_ = installation.copy_iso_network_config(
+				enable_services=True,  # Sources the ISO network configuration to the install medium.
+			)
+		case NicType.NM | NicType.NM_IWD:
+			packages = ['networkmanager']
 
-				installation.add_additional_packages(packages)
+			if network_config.type == NicType.NM:
+				packages.append('wpa_supplicant')
+			else:
+				packages.append('iwd')
 
-				# Desktop profile -> Always add applet
-				if profile_config and profile_config.profile:
-					if profile_config.profile.is_desktop_profile():
-						installation.add_additional_packages('network-manager-applet')
+			if profile_config and profile_config.profile:
+				if profile_config.profile.is_desktop_profile():
+					packages.append('network-manager-applet')
 
-				installation.enable_service('NetworkManager.service')
-				if network_config.type == NicType.NM_IWD:
-					# NM_IWD special handling
-					installation.configure_nm_iwd()
-					installation.disable_service('iwd.service')
+			installation.add_additional_packages(packages)
+			installation.enable_service('NetworkManager.service')
 
-			case NicType.MANUAL:
-				for nic in network_config.nics:
-					installation.configure_nic(nic)
-				installation.enable_service('systemd-networkd')
-				installation.enable_service('systemd-resolved')
+			if network_config.type == NicType.NM_IWD:
+				_configure_nm_iwd(installation)
+				installation.disable_service('iwd.service')
+
+		case NicType.MANUAL:
+			for nic in network_config.nics:
+				installation.configure_nic(nic)
+			installation.enable_service('systemd-networkd')
+			installation.enable_service('systemd-resolved')
+
+
+def _configure_nm_iwd(installation: Installer) -> None:
+	nm_conf_dir = installation.target / 'etc/NetworkManager/conf.d'
+	nm_conf_dir.mkdir(parents=True, exist_ok=True)
+
+	iwd_backend_conf = nm_conf_dir / 'wifi_backend.conf'
+	_ = iwd_backend_conf.write_text('[device]\nwifi.backend=iwd\n')
