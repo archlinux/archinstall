e8ea33c41c6eaaa220f091f6acfef7996f1e21b7
Softer <sft.nik@gmail.com>
2026-04-18 05:04:14 +0300

    Enable power management services after package installation (#4440)

--- archinstall/applications/power_management.py
+++ archinstall/applications/power_management.py
@@ -21,6 +21,18 @@ class PowerManagementApp:
 			'tuned-ppd',
 		]
 
+	@property
+	def ppd_services(self) -> list[str]:
+		return [
+			'power-profiles-daemon.service',
+		]
+
+	@property
+	def tuned_services(self) -> list[str]:
+		return [
+			'tuned.service',
+		]
+
 	def install(
 		self,
 		install_session: Installer,
@@ -31,5 +43,7 @@ class PowerManagementApp:
 		match power_management_config.power_management:
 			case PowerManagement.POWER_PROFILES_DAEMON:
 				install_session.add_additional_packages(self.ppd_packages)
+				install_session.enable_service(self.ppd_services)
 			case PowerManagement.TUNED:
 				install_session.add_additional_packages(self.tuned_packages)
+				install_session.enable_service(self.tuned_services)
