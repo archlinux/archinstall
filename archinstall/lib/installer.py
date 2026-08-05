86db19926ccf0cf3b20d491b60c0f47da135299c
Victor <victorzaca2003@gmail.com>
2026-08-05 14:58:38 -0300

    Log os-prober enablement during GRUB setup
    The os-prober block straps packages and edits /etc/default/grub without
    leaving any trace in the install log. Add a debug marker, matching the
    logging convention of the surrounding bootloader steps.

--- archinstall/lib/installer.py
+++ archinstall/lib/installer.py
@@ -1438,6 +1438,8 @@ class Installer:
 			grub_default.write_text(config)
 
 		if os_prober:
+			debug('Enabling os-prober in GRUB configuration')
+
 			# fuse3 enables grub-mount, which os-prober requires to inspect
 			# partitions that are not mounted (e.g. Windows on another disk)
 			self.pacman.strap(['os-prober', 'fuse3'])
