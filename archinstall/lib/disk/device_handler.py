5633ffa9fb51c2bf2a900a0669ea69e76b282866
Daniel Girtler <girtler.daniel@gmail.com>
2026-08-11 20:18:58 +1000

    Revert "feat(disk): add custom cipher selection for LUKS2 encryption (#4581)"
    This reverts commit 7b7fd6573e6513e87475de4017cf561af1fc9ced.

--- archinstall/lib/disk/device_handler.py
+++ archinstall/lib/disk/device_handler.py
@@ -20,13 +20,11 @@ from archinstall.lib.exceptions import DiskError, SysCallError, UnknownFilesyste
 from archinstall.lib.hardware import SysInfo
 from archinstall.lib.log import debug, error, info, log
 from archinstall.lib.models.device import (
-	DEFAULT_CIPHER,
 	DEFAULT_ITER_TIME,
 	BDevice,
 	BtrfsMountOption,
 	DeviceModification,
 	DiskEncryption,
-	EncryptionCipher,
 	FilesystemType,
 	LsblkInfo,
 	ModificationStatus,
@@ -284,7 +282,6 @@ class DeviceHandler:
 		enc_password: Password | None,
 		lock_after_create: bool = True,
 		iter_time: int = DEFAULT_ITER_TIME,
-		cipher: EncryptionCipher = DEFAULT_CIPHER,
 	) -> Luks2:
 		luks_handler = Luks2(
 			dev_path,
@@ -292,7 +289,7 @@ class DeviceHandler:
 			password=enc_password,
 		)
 
-		key_file = luks_handler.encrypt(iter_time=iter_time, cipher=cipher)
+		key_file = luks_handler.encrypt(iter_time=iter_time)
 
 		udev_sync()
 
@@ -323,7 +320,7 @@ class DeviceHandler:
 			password=enc_conf.encryption_password,
 		)
 
-		key_file = luks_handler.encrypt(iter_time=enc_conf.iter_time, cipher=enc_conf.cipher)
+		key_file = luks_handler.encrypt(iter_time=enc_conf.iter_time)
 
 		udev_sync()
 
@@ -334,8 +331,6 @@ class DeviceHandler:
 
 		info(f'luks2 formatting mapper dev: {luks_handler.mapper_dev}')
 		self.format(fs_type, luks_handler.mapper_dev)
-
-		udev_sync()
 
 		info(f'luks2 locking device: {dev_path}')
 		luks_handler.lock()
