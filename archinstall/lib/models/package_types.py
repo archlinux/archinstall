5c62da2708ed89424af133cd881f3d625d5f67b9
h8d13 <hadean-eon-dev@proton.me>
2026-08-12 13:35:18 +0200

    fixup! feat(kernels): add `RT` variants from upstream

--- archinstall/lib/models/package_types.py
+++ archinstall/lib/models/package_types.py
@@ -3,11 +3,12 @@ from typing import Final
 
 
 class Kernel(StrEnum):
-        LINUX = auto()
-        LINUX_LTS = 'linux-lts'
-        LINUX_ZEN = 'linux-zen'
-        LINUX_HARDENED = 'linux-hardened'
-        LINUX_RT = 'linux-rt'
-        LINUX_RT_LTS = 'linux-rt-lts'
+	LINUX = auto()
+	LINUX_LTS = 'linux-lts'
+	LINUX_ZEN = 'linux-zen'
+	LINUX_HARDENED = 'linux-hardened'
+	LINUX_RT = 'linux-rt'
+	LINUX_RT_LTS = 'linux-rt-lts'
+
 
 DEFAULT_KERNEL: Final = Kernel.LINUX
