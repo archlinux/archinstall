735f6cd2f19c90fdaa33e1b3d775e7e87e558c66
h8d13 <hadean-eon-dev@proton.me>
2026-08-12 13:32:41 +0200

    chore(fmt): use tab
    Signed-off-by: h8d13 <hadean-eon-dev@proton.me>

--- archinstall/lib/models/package_types.py
+++ archinstall/lib/models/package_types.py
@@ -3,10 +3,10 @@ from typing import Final
 
 
 class Kernel(StrEnum):
-	LINUX = auto()
-	LINUX_LTS = 'linux-lts'
-	LINUX_ZEN = 'linux-zen'
-	LINUX_HARDENED = 'linux-hardened'
+        LINUX = auto()
+        LINUX_LTS = 'linux-lts'
+        LINUX_ZEN = 'linux-zen'
+        LINUX_HARDENED = 'linux-hardened'
         LINUX_RT = 'linux-rt'
         LINUX_RT_LTS = 'linux-rt-lts'
 
